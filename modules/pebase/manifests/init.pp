ced60c93024c0d51a9d5221106425419811d5cd5
Hunter Haugen <h.haugen@gmail.com>
2011-08-06 17:11:47 +1000

    pebase module doesn't curl now

--- modules/pebase/manifests/init.pp
+++ modules/pebase/manifests/init.pp
@@ -1,8 +1,10 @@
 class pebase {
-  exec { 'Curl PE tarball':
-    command => '/usr/bin/curl -s -o /root/puppet-enterprise-1.1-centos-5-x86_64.tar http://pm.puppetlabs.com/puppet-enterprise/1.1/puppet-enterprise-1.1-centos-5-x86_64.tar',
-    creates => '/root/puppet-enterprise-1.1-centos-5-x86_64.tar',
-  }
+  ## Replaced with a file resource below
+  #exec { 'Curl PE tarball':
+  #  command => '/usr/bin/curl -so /root/puppet-enterprise-1.1-centos-5-x86_64.tar http://pm.puppetlabs.com/puppet-enterprise/1.1/puppet-enterprise-1.1-centos-5-x86_64.tar',
+  #  creates => '/root/puppet-enterprise-1.1-centos-5-x86_64.tar',
+  #}
+
   # do some basic setup for PE
   file { ['/etc/puppetlabs/',
           '/etc/puppetlabs/puppet/',
@@ -10,6 +12,9 @@ class pebase {
           '/etc/puppetlabs/puppet/manifests',
          ]:
     ensure => directory
+  }
+  file { '/root/puppet-enterprise-1.1-centos-5-x86_64.tar':
+    source => 'puppet:///modules/pebase/puppet-enterprise-1.1-centos-5-x86_64.tar',
   }
   file { '/root/dev-answers.txt':
     source => 'puppet:///modules/pebase/dev-answers.txt',
