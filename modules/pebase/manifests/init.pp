4ae7221a02b402de519c466a91b7b827f547968b
Hunter Haugen <h.haugen@gmail.com>
2011-11-18 11:16:32 -0800

    Remove pre-gen'd answer files

--- modules/pebase/manifests/init.pp
+++ modules/pebase/manifests/init.pp
@@ -13,18 +13,6 @@ class pebase {
          ]:
     ensure => directory
   }
-  #file { '/root/puppet-enterprise-2.0.0-el-5-i386.tar.gz':
-  #  source => 'puppet:///modules/pebase/puppet-enterprise-2.0.0-el-5-i386.tar.gz',
-  #}
-  file { '/root/dev-answers.txt':
-    source => 'puppet:///modules/pebase/dev-answers.txt',
-  }
-  file { '/root/pe-agent.answers':
-    source => 'puppet:///modules/pebase/pe-agent.answers',
-  }
-  file { '/root/pe-puppetmaster.answers':
-    source => 'puppet:///modules/pebase/pe-puppetmaster.answers',
-  }
   file { '/root/puppet-enterprise':
     ensure => symlink,
     target => '/root/puppet-enterprise-2.0.0-el-5-i386',
