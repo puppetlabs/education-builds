6f77b12c911600a8de96c0573815d138d421670d
Hunter Haugen <h.haugen@gmail.com>
2011-12-28 18:34:29 -0800

    Adding learning module for learning VM

---
+++ modules/learning/manifests/install.pp
@@ -0,0 +1,8 @@
+class learning::install {
+  exec {'install-pe':
+    command     => "/root/puppet-enterprise/puppet-enterprise-installer -D -a /root/learning.answers",
+    logoutput   => true,
+    environment => "RUBYLIB=''",
+    # If you don't reset the rubylib, it'll inherit the one used during kickstart and the installer will blow up.
+  }
+}
