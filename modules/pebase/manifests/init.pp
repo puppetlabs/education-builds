4237fe99c8ac080f57933637cf55e00a1a95c8ab
nfagerlund <nick.fagerlund@gmail.com>
2012-02-01 12:27:09 -0800

    Update to PE 2.0.1
    This commit creates VMs with PE 2.0.1, which includes an updated version of
    Puppet and several other improvements. I have opted not to wait for PE 2.0.2,
    as it is a regression-fix release that only affects Debian/Ubuntu and EL4
    platforms.

--- modules/pebase/manifests/init.pp
+++ modules/pebase/manifests/init.pp
@@ -1,12 +1,12 @@
 class pebase {
   ## Replaced with a file resource below
   #exec { 'Curl PE tarball':
-  #  command => '/usr/bin/curl -so /root/puppet-enterprise-2.0.0-el-5-i386.tar.gz http://pm.puppetlabs.com/puppet-enterprise/2.0.0/puppet-enterprise-2.0.0-el-5-i386.tar.gz',
-  #  creates => '/root/puppet-enterprise-2.0.0-el-5-i386.tar.gz',
+  #  command => '/usr/bin/curl -so /root/puppet-enterprise-2.0.1-el-5-i386.tar.gz http://pm.puppetlabs.com/puppet-enterprise/2.0.1/puppet-enterprise-2.0.1-el-5-i386.tar.gz',
+  #  creates => '/root/puppet-enterprise-2.0.1-el-5-i386.tar.gz',
   #}
 
   file { '/root/puppet-enterprise':
     ensure => symlink,
-    target => '/root/puppet-enterprise-2.0.0-el-5-i386',
+    target => '/root/puppet-enterprise-2.0.1-el-5-i386',
   }
 }
