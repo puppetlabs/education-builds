40a9af41218232263d5fd02e434c8e3e9ac27a77
Ryan Coleman <ryan@puppetlabs.com>
2012-06-26 18:35:28 -0700

    Major refactoring of some Apache labs
    This commit has several large changes to the apache labs in order
    to make them feed off each other better and flow better for the course.
    What you don't see in the slides is the exercises manual we hacked up
    in a google doc for tomorrows course.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,5 +1,6 @@
 class edu_bootstrap::agent {
 
+  require edu_bootstrap
   require edu_bootstrap::repo
 
   package { 'fuse-sshfs':
@@ -8,6 +9,13 @@ class edu_bootstrap::agent {
 
   file { '/root/master_home':
     ensure => directory,
+  }
+
+  concat::fragment{ "apply_modulepath":
+    target  => '/etc/puppetlabs/puppet/puppet.conf',
+    content => "[user]\n  modulepath=/root/master_home/modules\n",
+    order   => '02',
+    require => Concat::Fragment['puppet_conf'],
   }
 
 }
