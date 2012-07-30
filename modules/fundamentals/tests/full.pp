1c42f16b1300abf43b43e84ac3d0f3fc56d01334
Zack Smith <zack@puppetlabs.com>
2012-07-30 16:24:05 -0700

    Renamed Example_usage

---
+++ modules/fundamentals/tests/full.pp
@@ -0,0 +1,6 @@
+# On a PE node, build out an example setup.
+# $student_array is for passing a direct array
+# $students is what you use in the console, to pass a comma delmited string
+
+$student_array = [ 'ryan', 'zack', 'ben', 'brad', 'ralph' ]
+include edu_bootstrap
