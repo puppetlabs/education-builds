634eefb74dcef7ba1e38bfc59847da46e530ab98
Zack Smith <zack@puppetlabs.com>
2012-08-20 10:59:29 -0700

    Attempt to fix environment removal
    Students are seeing their environment line being removed from
    puppet.conf, normally just after the first run ( or day 2 ).

--- modules/fundamentals/manifests/init.pp
+++ modules/fundamentals/manifests/init.pp
@@ -8,34 +8,33 @@ class fundamentals {
   $is_puppetconsole = $::fact_is_puppetconsole ? { 'true'  => true, 'false' => false }
   $is_puppetagent = $::fact_is_puppetagent ? { 'true'  => true, 'false' => false }
 
-  concat{ 'puppet_conf_concat':
-    name  => '/etc/puppetlabs/puppet/puppet.conf',
-    owner => 'pe-puppet',
-    group => 'pe-puppet',
-    mode  => '0644',
-  }
-
-  concat::fragment{ 'puppet_conf':
-    target  => '/etc/puppetlabs/puppet/puppet.conf',
-    source  => '/root/puppet.conf',
-    order   => 01,
-  }
-
-  exec { 'cp_puppet_conf':
-    command => '/bin/cp /etc/puppetlabs/puppet/puppet.conf /root/puppet.conf',
-    unless  => '/usr/bin/test -f /root/puppet.conf',
-    before  => Concat::Fragment['puppet_conf'],
-  }
-
 
   if $is_puppetmaster {
+
+    concat{ 'puppet_conf_concat':
+      name  => '/etc/puppetlabs/puppet/puppet.conf',
+      owner => 'pe-puppet',
+      group => 'pe-puppet',
+      mode  => '0644',
+    }
+
+    concat::fragment{ 'puppet_conf':
+      target  => '/etc/puppetlabs/puppet/puppet.conf',
+      source  => '/root/puppet.conf',
+      order   => 01,
+    }
+
+    exec { 'cp_puppet_conf':
+      command => '/bin/cp /etc/puppetlabs/puppet/puppet.conf /root/puppet.conf',
+      unless  => '/usr/bin/test -f /root/puppet.conf',
+      before  => Concat::Fragment['puppet_conf'],
+    }
 
     $student_array = split($::students, ',')
     fundamentals::user { $student_array: }
 
     $class_array = split($::classes, ',')
     fundamentals::console_class { $class_array: }
-    include fundamentals::nfs::server
   }
 
   if $is_puppetagent and ! $is_puppetmaster {
