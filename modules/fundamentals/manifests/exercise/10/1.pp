8c592e6ea987b5fe17980852901a8df4b078b9df
James Sweeny <james.sweeny@puppetlabs.com>
2012-12-16 12:33:31 -0500

    Remove unnecessary execs and notifies

--- modules/fundamentals/manifests/exercise/10/1.pp
+++ modules/fundamentals/manifests/exercise/10/1.pp
@@ -1,18 +1,3 @@
 class fundamentals::exercise::10::1 inherits fundamentals::exercise::9::2 {
   $exercise_version = '10.1'
-  exec { 'facter ipaddress':
-    command   => 'facter ipaddress',
-    logoutput => true,
-    path      => '/usr/local/bin'
-  }
-  exec { 'facter operatingsystem':
-    command   => 'facter operatingsystem',
-    logoutput => true,
-    path      => '/usr/local/bin'
-  }
-  exec { 'facter':
-    command   => 'facter',
-    logoutput => true,
-    path      => '/usr/local/bin'
-  }
 }
--- modules/fundamentals/manifests/exercise/7/1.pp
+++ modules/fundamentals/manifests/exercise/7/1.pp
@@ -1,4 +1,3 @@
 class fundamentals::exercise::7::1 inherits fundamentals::exercise::6::1 {
   $exercise_version = '7.1'
-  notify { 'Login to the enterprise console and view reports': }
 }
