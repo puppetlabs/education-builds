65bc5b01d2d7830dfcf294197b1de06dec425ba0
Ben Ford <ben.ford@puppetlabs.com>
2013-01-04 17:36:25 -0800

    Added the modules for the extending capstone
    Yes, I committed a binary file. It's an upstream very stable project and
    I don't imagine that we'll ever make local changes. Also, it's something
    like 2.5 mb uncompressed. Boo.
    
    Anyways.
    
    Classify a node with 'extending::capstone' and you will get:
    
      * Apache
      * Mysql
      * PHP w/ bindings
      * Drupal & modules I'm using
      * Drupal command line management interface
      * Custom modules to provide:
        * A nifty hiera graphical source:key:value manager
        * A REST API to query values
        * A REST API to post simplified reports viewable from the standard
          Drupal log interface
      * Drupal configured with an admin user. (admin/puppet)
    
    I still have a bit of documentation to work on for the lab. As in a lot.
    
    This requires the packages mentioned in https://projects.puppetlabs.com/issues/18374

---
+++ modules/extending/manifests/capstone.pp
@@ -0,0 +1,51 @@
+class extending::capstone {
+  include apache
+  include apache::mod::php
+  include mysql::server
+
+  package { 'php-mysql':
+    ensure => present,
+  }
+
+  class { 'drupal':
+    require => [ Class['apache::mod::php', 'mysql::server'], Package['php-mysql'] ],
+  }
+  
+  package { ['drupal7-ctools', 'drupal7-features', 'drupal7-views']:
+    ensure  => present,
+    require => Class['drupal'],
+    before  => Exec['enable modules'],
+  }
+  
+  $module_path = '/usr/share/drupal7/sites/all/modules/'
+  file { 'puppet_report':
+    ensure  => directory,
+    path    => "${module_path}/puppet_report",
+    source  => 'puppet:///modules/extending/capstone/puppet_report',
+    recurse => true,
+    require => Class['drupal'],
+  }
+  
+  file { 'datasource':
+    ensure  => directory,
+    path    => "${module_path}/datasource",
+    source  => 'puppet:///modules/extending/capstone/datasource',
+    recurse => true,
+    require => Class['drupal'],
+  }
+  
+  file { 'composed_field':
+    ensure  => directory,
+    path    => "${module_path}/composed_field",
+    source  => 'puppet:///modules/extending/capstone/composed_field',
+    recurse => true,
+    require => Class['drupal'],
+  }
+
+  exec { 'enable modules':
+    command => 'drush pm-enable puppet_report datasource_api -y', # enables dependencies
+    path    => '/usr/local/bin:/bin:/usr/bin',
+    require => File['puppet_report', 'datasource', 'composed_field'],
+  }
+
+}
---
+++ modules/extending/tests/capstone.pp
@@ -0,0 +1,2 @@
+include extending::capstone
+
