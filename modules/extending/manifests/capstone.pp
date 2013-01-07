1dcbacf0222b3cfa159b45a41437e016b7597b26
Ben Ford <ben.ford@puppetlabs.com>
2013-01-07 14:59:32 -0800

    Cleaned up my capstone module.
    This caches the mysql password so that it actually works with the
    console database and doesn't blow things away.
    
    Also cleans up the module so it's readable.

--- modules/extending/manifests/capstone.pp
+++ modules/extending/manifests/capstone.pp
@@ -1,54 +1,15 @@
 class extending::capstone {
-  include apache
-  include apache::mod::php
-  include mysql::server
 
-  package { 'php-mysql':
-    ensure => present,
-  }
+  class { 'extending::capstone::requirements':
+
+  } ->
 
   class { 'drupal':
-    require => [ Class['apache::mod::php', 'mysql::server'], Package['php-mysql'] ],
-  }
 
-  package { ['drupal7-ctools', 'drupal7-features', 'drupal7-views']:
-    ensure  => present,
-    require => Class['drupal'],
-    notify  => Exec['enable modules'],
-  }
+  } ->
 
-  $module_path = '/usr/share/drupal7/sites/all/modules/'
-  file { 'puppet_report':
-    ensure  => directory,
-    path    => "${module_path}/puppet_report",
-    source  => 'puppet:///modules/extending/capstone/puppet_report',
-    recurse => true,
-    require => Class['drupal'],
-    notify  => Exec['enable modules'],
-  }
+  class { 'extending::capstone::modules':
 
-  file { 'datasource':
-    ensure  => directory,
-    path    => "${module_path}/datasource",
-    source  => 'puppet:///modules/extending/capstone/datasource',
-    recurse => true,
-    require => Class['drupal'],
-    notify  => Exec['enable modules'],
-  }
-
-  file { 'composed_field':
-    ensure  => directory,
-    path    => "${module_path}/composed_field",
-    source  => 'puppet:///modules/extending/capstone/composed_field',
-    recurse => true,
-    require => Class['drupal'],
-    notify  => Exec['enable modules'],
-  }
-
-  exec { 'enable modules':
-    command     => 'drush pm-enable puppet_report datasource_api -y', # enables dependencies
-    path        => '/usr/local/bin:/bin:/usr/bin',
-    refreshonly => true,
   }
 
 }
