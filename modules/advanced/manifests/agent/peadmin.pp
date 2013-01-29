d736b5bbb1516dc282059c90e61ac658bc6d24a5
Zack Smith <zack@puppetlabs.com>
2013-01-29 10:14:46 -0800

    whitespace

--- modules/advanced/manifests/agent/peadmin.pp
+++ modules/advanced/manifests/agent/peadmin.pp
@@ -1,6 +1,6 @@
 class advanced::agent::peadmin {
- file {'/var/lib/peadmin/.mcollective':
-   owner   => 'peadmin',
-   content => file('/etc/puppetlabs/puppet/.mcollective_advanced_class'),
- }
+  file {'/var/lib/peadmin/.mcollective':
+    owner   => 'peadmin',
+    content => file('/etc/puppetlabs/puppet/.mcollective_advanced_class'),
+  }
 }
