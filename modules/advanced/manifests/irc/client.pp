af438b459ad55d6adad45348b95e629b4584a141
Zack Smith <zack@puppetlabs.com>
2013-01-29 10:28:47 -0800

    Make irssi config self contained

--- modules/advanced/manifests/irc/client.pp
+++ modules/advanced/manifests/irc/client.pp
@@ -1,3 +1,15 @@
+# This class configures the irssi client settings
 class advanced::irc::client {
-  include irssi
+  package { 'irssi':
+    ensure => present,
+  }
+  file { "${::root_home}/.irssi":
+    ensure  => directory,
+    require => Package['irssi'],
+  }
+  file { "${::root_home}/.irssi/config":
+    ensure  => present,
+    content => template("${module_name}/.irssi/config/irssi.conf.erb"),
+    require => Package['irssi'],
+  }
 }
