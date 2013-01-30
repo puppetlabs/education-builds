554ec719b353ae5a10c1e87071d42d058e60c96b
Zack Smith <zack@puppetlabs.com>
2013-01-30 12:18:45 -0800

    Temp fix for 2.7.0 puppetdb bug
    Prior to this commit 2.7.0 would error both because puppetbd is
    redeclaring `service pe-httpd` and `file .../auth.conf` I am removing
    these classes right now in attempt to help solve the problem until it
    can be fixed in the code.

--- modules/advanced/manifests/classroom/puppetdb.pp
+++ modules/advanced/manifests/classroom/puppetdb.pp
@@ -11,4 +11,16 @@ class advanced::classroom::puppetdb {
     terminus_package    => 'pe-puppetdb-terminus',
     puppet_service_name => 'pe-httpd',
   }
+
+  # This is a temp fix for 2.7.0 puppetdb bug
+  $request_manager = 'request_manager'
+  exec { "nodeclass:del ${request_manager}":
+    path        => '/opt/puppet/bin:/bin',
+    cwd         => '/opt/puppet/share/puppet-dashboard',
+    environment => 'RAILS_ENV=production',
+    command     => "rake nodeclass:del name=${request_manager}",
+    onlyif      => "rake RAILS_ENV=production nodeclass:list | grep ${request_manager}",
+    returns     => '1',
+  }
+
 }
