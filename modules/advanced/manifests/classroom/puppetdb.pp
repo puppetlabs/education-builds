f22d072e4e649e6010eae36fa4b9bae915b25d9b
Zack Smith <zack@puppetlabs.com>
2013-01-30 13:25:18 -0800

    Up the puppetdb startup timeout
    Prior to this commit we only used the the default 15 second timeout this
    causes issue with the intial puppetdb configuration on the instructor vm
    causing two runs to be required.

--- modules/advanced/manifests/classroom/puppetdb.pp
+++ modules/advanced/manifests/classroom/puppetdb.pp
@@ -6,10 +6,11 @@ class advanced::classroom::puppetdb {
     confdir          => '/etc/puppetlabs/puppetdb/conf.d',
   }
   class { 'puppetdb::master::config':
-    puppetdb_server     => 'classroom.puppetlabs.vm',
-    puppet_confdir      => '/etc/puppetlabs/puppet',
-    terminus_package    => 'pe-puppetdb-terminus',
-    puppet_service_name => 'pe-httpd',
+    puppetdb_server          => 'classroom.puppetlabs.vm',
+    puppet_confdir           => '/etc/puppetlabs/puppet',
+    terminus_package         => 'pe-puppetdb-terminus',
+    puppet_service_name      => 'pe-httpd',
+    puppetdb_startup_timeout => '60',
   }
 
   # This is a temp fix for 2.7.0 puppetdb bug
