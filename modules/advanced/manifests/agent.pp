b8f1af2586105b1e519f2db2b10918c7cc6fe303
Carthik Sharma <carthik@puppetlabs.com>
2013-04-04 12:57:01 -0700

    Adding logic essential for agents with PE < 2.7.2 installed

--- modules/advanced/manifests/agent.pp
+++ modules/advanced/manifests/agent.pp
@@ -4,4 +4,11 @@ class advanced::agent {
   class {'advanced::agent::puppetdb':} ->
   class {'advanced::agent::peadmin':} ->
   class {'advanced::irc::client':}
+  $advanced_pe_version = adv_pe_ver()
+  
+  # If this agent has PE < 2.7.2, we need to copy over some ssl certs 
+  # and the mcollective credentials file
+  if versioncmp($advanced_pe_version, '2.7.2') < 0 {
+    include advanced::agent::mcofiles
+  }
 }
