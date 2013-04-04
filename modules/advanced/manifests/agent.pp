421facdbec28274428905154d4b54a7157bcd576
Carthik Sharma <carthik@puppetlabs.com>
2013-04-04 15:47:23 -0700

    Fixed the comment in the manifest

--- modules/advanced/manifests/agent.pp
+++ modules/advanced/manifests/agent.pp
@@ -6,7 +6,7 @@ class advanced::agent {
   class {'advanced::irc::client':}
   $advanced_pe_version = adv_pe_ver()
   
-  # If this agent has PE < 2.7.2, we need to copy over some ssl certs 
+  # If this agent has PE < 2.7.1, we need to copy over some ssl certs 
   # and the mcollective credentials file
   if versioncmp($advanced_pe_version, '2.7.1') < 0 {
     include advanced::agent::mcofiles
