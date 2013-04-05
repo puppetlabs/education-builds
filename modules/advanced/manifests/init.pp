9f8d52c1f94f097de8159dc5574c0ed4dfba46e3
Carthik Sharma <carthik@puppetlabs.com>
2013-04-04 17:05:44 -0700

    Added version check for PE on classroom
    Due to how pe_mcollective has changed, you require PE >=2.7.1 on
    classroom.puppetlabs.vm

--- modules/advanced/manifests/init.pp
+++ modules/advanced/manifests/init.pp
@@ -1,7 +1,11 @@
 # This is the class applied to all systems
 class advanced {
   if    $::hostname == 'classroom' {
-    include advanced::classroom
+    if versioncmp( $::pe_version, 2.7.1) < 0 {
+      fail ("The version of PE installed on ${::fqdn} is ${::pe_version}. You need PE > 2.7.1 installed on ${::fqdn}!")
+    } else {
+      include advanced::classroom
+    }
   }
   elsif $::hostname == 'proxy' {
     include advanced::proxy
