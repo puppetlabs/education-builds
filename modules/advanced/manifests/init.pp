da2975122eada0632e9d7f4d7ab51861c7242c9b
Carthik Sharma <carthik@puppetlabs.com>
2013-04-04 17:40:06 -0700

    Missed some quotes, again.

--- modules/advanced/manifests/init.pp
+++ modules/advanced/manifests/init.pp
@@ -1,7 +1,7 @@
 # This is the class applied to all systems
 class advanced {
-  if    $::hostname == 'classroom' {
-    if versioncmp( $::pe_version, 2.7.1) < 0 {
+  if  $::hostname == 'classroom' {
+    if versioncmp($::pe_version, '2.7.1') < 0 {
       fail ("The version of PE installed on ${::fqdn} is ${::pe_version}. You need PE > 2.7.1 installed on ${::fqdn}!")
     } else {
       include advanced::classroom
