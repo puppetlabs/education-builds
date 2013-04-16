46c3e4667745d667826b6c57a7c233df8574e58e
Carthik Sharma <carthik@puppetlabs.com>
2013-04-16 11:03:30 -0700

    stomp_password not stomp_credentials
    Renamed variable so it works.

--- modules/advanced/manifests/mcollective.pp
+++ modules/advanced/manifests/mcollective.pp
@@ -4,7 +4,7 @@ class advanced::mcollective {
   # The susbcribe is to ensure it only runs once -
   # The first time agent runs after classifying classroom with advanced
   $stomp_server = 'classroom.puppetlabs.vm'
-  $stomp_credentials = file('/etc/puppetlabs/mcollective/credentials')
+  $stomp_password = file('/etc/puppetlabs/mcollective/credentials')
 
   Exec {
     path        => '/opt/puppet/bin:/bin',
@@ -19,8 +19,8 @@ class advanced::mcollective {
     command     => "rake nodegroup:parameters name=default parameters=fact_stomp_server=${stomp_server}",
   }
 
-  exec { 'node:parameters:stomp_credentials':
-    command     => "rake nodegroup:parameters name=default parameters=stomp_credentials=${stomp_credentials}",
+  exec { 'node:parameters:stomp_password':
+    command     => "rake nodegroup:parameters name=default parameters=stomp_password=${stomp_password}",
   }
 
   exec { 'node:parameters:fact_is_puppetmaster':
