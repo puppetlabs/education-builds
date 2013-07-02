5c92e08adea818a8bb61140e71ae4efe179d563c
Ben Ford <ben.ford@puppetlabs.com>
2013-07-02 11:53:47 -0700

    refactored functions and tests out

---
+++ scripts/classroom/functions.sh
@@ -0,0 +1,40 @@
+NAME=$1
+COLUMNS=$(tput cols)
+WIDTH=$[COLUMNS - 12]
+
+if [ $# -ne 1 ]
+then
+  echo "Please call this script with the username you provided to the instructor."
+  echo "For example, ${0} <myname>"
+  exit 1
+fi
+
+function success ()
+{
+  MESSAGE="$1"
+  printf "%-${WIDTH}s[\033[32m  OK  \033[0m]\n" "$MESSAGE"
+}
+
+function fail ()
+{
+  MESSAGE="$1"
+  RESOLUTION="$2"
+  printf "%-${WIDTH}s[\033[31m FAIL \033[0m]\n" "$MESSAGE"
+  printf "  > %s\n" "$RESOLUTION"
+}
+
+function check ()
+{
+  COMMAND="$1"
+  MESSAGE="$2"
+  RESOLUTION="$3"
+
+  $COMMAND > /dev/null 2>&1
+
+  if [ $? -eq 0 ]
+  then
+    success "$MESSAGE"
+  else
+    fail "$MESSAGE" "$RESOLUTION"
+  fi
+}
