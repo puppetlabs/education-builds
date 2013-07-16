adb72ea8751efaa15185c1e988da464331048cdc
Carthik Sharma <carthik@puppetlabs.com>
2013-07-15 20:26:47 -0700

    PuppetDB fixes
      - conditionally include puppetdb if PE < 3.0.0
      - remove the rake task to remove request_manager since
        that was only needed for PE 2.7.0 and this modules
        supports only PE > 2.7.0 according to init.pp

--- modules/advanced/manifests/classroom/puppetdb.pp
+++ modules/advanced/manifests/classroom/puppetdb.pp
@@ -14,15 +14,4 @@ class advanced::classroom::puppetdb {
     puppetdb_startup_timeout => '60',
   }
 
-  # This is a temp fix for 2.7.0 puppetdb bug
-  $request_manager = 'request_manager'
-  exec { "nodeclass:del ${request_manager}":
-    path        => '/opt/puppet/bin:/bin',
-    cwd         => '/opt/puppet/share/puppet-dashboard',
-    environment => 'RAILS_ENV=production',
-    command     => "rake nodeclass:del name=${request_manager}",
-    onlyif      => "rake RAILS_ENV=production nodeclass:list | grep ${request_manager}",
-    returns     => '1',
-  }
-
 }
