49a56ec378fabb0513b17e3ceda18381182db27e
Carthik Sharma <carthik@puppetlabs.com>
2013-07-26 11:44:46 -0700

    backup all the files, replace some. Others have changes to lines

--- modules/advanced/manifests/classroom.pp
+++ modules/advanced/manifests/classroom.pp
@@ -3,13 +3,20 @@ class advanced::classroom {
   include kickstand
   include advanced::classroom::fileserver
   # These are the files that we manage with this class
-  $managed_files = [
+  $files_to_backup = [
     '/etc/puppetlabs/puppet/auth.conf',
     '/etc/puppetlabs/puppet/manifests/site.pp',
     '/etc/puppetlabs/console-auth/config.yml',
     '/etc/puppetlabs/rubycas-server/config.yml',
     '/etc/puppetlabs/puppetdb/conf.d/jetty.ini',
   ]
+  
+  $files_to_replace = [
+    '/etc/puppetlabs/puppet/auth.conf',
+    '/etc/puppetlabs/puppet/manifests/site.pp',
+  ]
+
+  if versioncmp($::pe_version, '3.0.0') < 1 {
 
   if versioncmp($::pe_version, '3.0.0') < 1 {
     class {'advanced::classroom::puppetdb':} 
@@ -22,9 +29,9 @@ class advanced::classroom {
     class {'advanced::classroom::console':}
   }
 
-  # Backup and create default file then don't replace
-  advanced::backup   { $managed_files :} ->
-  advanced::template { $managed_files :
+  # Backup files and create default file then don't replace
+  advanced::backup   { $files_to_backup: } ->
+  advanced::template { $files_to_replace :
     replace_file => false,
   }
   file { '/etc/puppetlabs/puppet/.mcollective_advanced_class':
