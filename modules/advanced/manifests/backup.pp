16ed5444e24bb7ca9509f2a641c152f55a835239
Carthik Sharma <carthik@puppetlabs.com>
2013-07-26 11:53:13 -0700

    backup implies copy, not move

--- modules/advanced/manifests/backup.pp
+++ modules/advanced/manifests/backup.pp
@@ -1,12 +1,12 @@
 # This is a helper class used with advanced::template
 define advanced::backup(
-  $path_to_move = $title,
+  $path_to_copy = $title,
 ){
 
-  # Idempotence is accomlished via the .old file extance
-  exec { "mv -f ${path_to_move} ${path_to_move}.old" :
+  # Idempotence is accomlished via the .old file 
+  exec { "cp -a ${path_to_copy} ${path_to_copy}.old" :
     path    => '/bin:/usr/bin',
-    creates => "${path_to_move}.old",
+    creates => "${path_to_copy}.old",
   }
 
 }
