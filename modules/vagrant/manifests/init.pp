db6a3eb736faab96ebef9f16f578f8db8b7de42e
Zack Smith <zack@puppetlabs.com>
2013-08-07 14:09:28 -0700

    Manage vagrant user's home directory
    Prior to this commit we did not manage the vagrant users home directory
    This commit adds a home that won't show up in class but will be created
    for the ssh key to live in

--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -5,7 +5,9 @@ class vagrant {
  $pub_key = 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ=='
 
   user { 'vagrant':
-    ensure => present,
+    ensure     => present,
+    home       => '/var/vagrant',
+    managehome => true,
   }
 
   ssh_authorized_key { 'vagrant public key':
