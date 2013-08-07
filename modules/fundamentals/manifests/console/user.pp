0a6e18b0a40dfca10a5cf8343a38937327408b18
Carthik Sharma <carthik@puppetlabs.com>
2013-08-07 15:44:01 -0700

    change cwd for the rake task depending on PE version

--- modules/fundamentals/manifests/console/user.pp
+++ modules/fundamentals/manifests/console/user.pp
@@ -15,13 +15,15 @@ define fundamentals::console::user ( $password ) {
 
   if versioncmp($::fundamentals_pe_version, '3.0.0') < 0 {
     $execute_me = 'rake db:create_user '
+    $current_dir = '/opt/puppet/share/console-auth'
    } else {
     $execute_me = 'bundle exec rake -f /opt/puppet/share/console-auth/Rakefile db:create_user '
+    $current_dir = '/opt/puppet/share/puppet-dashboard'
    }
 
   exec { "add_console_user_${name}":
     path    => '/opt/puppet/bin:/usr/bin',
-    cwd     => '/opt/puppet/share/console-auth',
+    cwd     => $current_dir,
     command => "${execute_me} ${userstring}=${name}@puppetlabs.com PASSWORD=${password} ROLE=Read-Write",
     unless  => "test -d /home/${name}",
     before  => File["/home/${name}"],
