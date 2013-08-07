bdc378d466aad07e9ee56510bb638f434c070e0e
Zack Smith <zack@puppetlabs.com>
2013-08-07 13:33:04 -0700

    Add vagrant module
    Prior to this commit , we did not add a vagrant user or ssh authorized
    key This commit adds a module which adds the user and the key. They key
    is the generic insecure public key from vagrant so it works out of the
    box. This should only be an issue if people use our VMs in production
    which should never happen. Also I suppose is students want to ssh into
    each other's vms but that can be done with the same password anyway out
    of the box

---
+++ modules/vagrant/manifests/init.pp
@@ -0,0 +1,18 @@
+class vagrant {
+  # Vagrant public key
+  # https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+
+ $pub_key = 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ=='
+
+  user { 'vagrant':
+    ensure => present,
+  }
+
+  ssh_authorized_key { 'vagrant public key':
+    ensure => present,
+    key    => $vagrant_key,
+    name   => 'vagrant insecure public key',
+    user   => 'vagrant',
+    type   => 'ssh-rsa',
+  }
+}
