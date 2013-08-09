099b292ab2e46e658696c98e536d6db457bb08a5
Zack Smith <zack@puppetlabs.com>
2013-08-08 17:59:27 -0700

    Add export

--- scripts/advanced_install.sh
+++ scripts/advanced_install.sh
@@ -22,26 +22,26 @@ export RUBYLIB=/usr/src/puppet/lib:/usr/src/facter/lib
 
 unset RUBYLIB
 
-q_all_in_one_install=n
-q_database_install=n
-q_install=y
-q_pe_database=n
-q_puppet_cloud_install=n
-q_puppet_enterpriseconsole_install=n
-q_puppet_symlinks_install=y
-q_puppetagent_certname="$MASTER_HOSTNAME"
-q_puppetagent_install=y
-q_puppetagent_server="$MASTER_HOSTNAME"
-q_puppetdb_hostname="$CLASSROOM_HOSTNAME"
-q_puppetdb_install=n
-q_puppetdb_port=8081
-q_puppetmaster_certname="$MASTER_HOSTNAME"
-q_puppetmaster_dnsaltnames="${MASTER_HOSTNAME/.*/},$MASTER_HOSTNAME,puppet,puppet.puppetlabs.vm"
-q_puppetmaster_enterpriseconsole_hostname="$CLASSROOM_HOSTNAME"
-q_puppetmaster_enterpriseconsole_port=443
-q_puppetmaster_install=y
-q_run_updtvpkg=n
-q_vendor_packages_install=y
+export q_all_in_one_install=n
+export q_database_install=n
+export q_install=y
+export q_pe_database=n
+export q_puppet_cloud_install=n
+export q_puppet_enterpriseconsole_install=n
+export q_puppet_symlinks_install=y
+export q_puppetagent_certname="$MASTER_HOSTNAME"
+export q_puppetagent_install=y
+export q_puppetagent_server="$MASTER_HOSTNAME"
+export q_puppetdb_hostname="$CLASSROOM_HOSTNAME"
+export q_puppetdb_install=n
+export q_puppetdb_port=8081
+export q_puppetmaster_certname="$MASTER_HOSTNAME"
+export q_puppetmaster_dnsaltnames="${MASTER_HOSTNAME/.*/},$MASTER_HOSTNAME,puppet,puppet.puppetlabs.vm"
+export q_puppetmaster_enterpriseconsole_hostname="$CLASSROOM_HOSTNAME"
+export q_puppetmaster_enterpriseconsole_port=443
+export q_puppetmaster_install=y
+export q_run_updtvpkg=n
+export q_vendor_packages_install=y
 
 # Run the master installation with an empty file given the exports above
 exec /root/puppet-enterprise/puppet-enterprise-installer -a /etc/motd
