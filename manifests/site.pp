717c9b1b27b29f32431985ac488633fb8cbf4971
Carthik Sharma <carthik@puppetlabs.com>
2013-08-12 15:39:12 -0700

    pebase removed since its useless now

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,13 @@
 $pe_version = '3.0.1'
 node default {
   include bootstrap
-  include pebase
-
   include localrepo
   include training
-
   include vagrant
 }
 
 node /learn/ {
   class { 'bootstrap': print_console_login => true, }
-  include pebase
-
   include learning
   stage { 'pe_install': require => Stage['main'], }
   class { 'learning::install': stage => pe_install, }
--- modules/pebase/manifests/init.pp
+++
@@ -1,6 +0,0 @@
-class pebase {
-  #file { '/root/puppet-enterprise':
-  #  ensure => symlink,
-  #  target => "/root/puppet-enterprise-${::pe_version}-el-6-i386",
-  #}
-}
