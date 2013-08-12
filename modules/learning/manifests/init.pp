b864c556bc97da835f81b62e5b5af2ad0a417994
Carthik Sharma <carthik@puppetlabs.com>
2013-08-12 16:22:29 -0700

    learning module updates checking in

--- modules/learning/manifests/init.pp
+++ modules/learning/manifests/init.pp
@@ -10,4 +10,23 @@ class learning {
     source => 'puppet:///modules/learning/learning.answers',
   }
 
+  # Print this info when we log in, too.
+  file {'/etc/motd':
+    ensure => file,
+    owner  => root,
+    mode   => 0644,
+    content => "Welcome to the Learning Puppet VM! To learn how to write Puppet code, go to
+http://docs.puppetlabs.com/learning and follow along.
+
+To view your current IP address, run `facter ipaddress_eth0`
+
+To log in to the Puppet Enterprise console, go to:
+https://<YOUR IP ADDRESS HERE>
+
+  User: puppet@example.com
+  Password: learningpuppet
+",
+  }
+
+
 }
--- modules/learning/manifests/install.pp
+++ modules/learning/manifests/install.pp
@@ -9,9 +9,36 @@ class learning::install {
 
   # This rake task exists now! Hurray.
   exec {'reduce-activemq-heap':
-    command     => '/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile node:parameters name=learn.localdomain parameters="activemq_heap_mb=256" RAILS_ENV=production',
+    command     => '/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile node:variables name="learn.localdomain" variables="activemq_heap_mb=256" RAILS_ENV=production',
     logoutput   => true,
     environment => "RUBYLIB=''",
     require     => Exec['install-pe'],
   }
+
+  # So we'll make sure it exists:
+  exec {'ensure learn.localdomain exists in console':
+    command     => '/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile node:add name="learn.localdomain" RAILS_ENV=production',
+    returns     => [0,1], # It returns 1 if the node already exists, but the command is actually idempotent, so that's fine.
+    logoutput   => true,
+    environment => "RUBYLIB=''",
+    require     => Exec['install-pe'],
+    before      => Exec['reduce-activemq-heap'],
+  }
+
+  # Add script that can print console login. Bootstrap will optionally call this in the rc.local file.
+  file {'/root/.console_login.sh':
+    ensure => file,
+    source => 'puppet:///modules/learning/console_login.sh',
+    mode   => 0755,
+  }
+
+  # Put examples in place -- we should have some way to automatically get the
+  # most recent from the puppet docs source, where they'll be in
+  # source/learning/files/examples.
+  file {'/root/examples':
+    ensure => directory,
+    source => "puppet:///modules/$module_name/examples",
+    recurse => true,
+  }
+
 }
