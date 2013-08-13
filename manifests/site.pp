8178dbf30297f2d57ccc81dc4c4a9fffd1b10db5
Carthik Sharma <carthik@puppetlabs.com>
2013-08-12 17:42:55 -0700

    cache packages locally for learning VM too

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,7 @@ node default {
 
 node /learn/ {
   class { 'bootstrap': print_console_login => true, }
+  include localrepo
   include learning
   stage { 'pe_install': require => Stage['main'], }
   class { 'learning::install': stage => pe_install, }
