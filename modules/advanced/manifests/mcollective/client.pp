f186214fb07573d69822b823c715041ede264968
Ben Ford <ben.ford@puppetlabs.com>
2013-10-10 12:50:51 -0700

    Only sync files if the peadmin user exists
    Fixes #TRAINVM-31

--- modules/advanced/manifests/mcollective/client.pp
+++ modules/advanced/manifests/mcollective/client.pp
@@ -12,17 +12,20 @@ class advanced::mcollective::client {
     }
   }
 
-  advanced::mcollective::file { $advanced::mcollective::config::ssl_certs:
-    ensure => file,
-    owner  => 'pe-puppet',
-    group  => 'pe-puppet',
-    mode   => '640',
-  }
+  # I don't like this check method, but it's the best we've got for now
+  if str2bool($::has_peadmin) {
+    advanced::mcollective::file { $advanced::mcollective::config::ssl_certs:
+      ensure => file,
+      owner  => 'pe-puppet',
+      group  => 'pe-puppet',
+      mode   => '640',
+    }
 
-  advanced::mcollective::file { $advanced::mcollective::config::peadmin:
-    ensure => file,
-    owner  => 'peadmin',
-    group  => 'peadmin',
-    mode   => '644',
+    advanced::mcollective::file { $advanced::mcollective::config::peadmin:
+      ensure => file,
+      owner  => 'peadmin',
+      group  => 'peadmin',
+      mode   => '644',
+    }
   }
 }
