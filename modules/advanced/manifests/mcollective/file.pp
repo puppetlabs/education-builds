90d477a951f7c7db095e084b0a7f663b08ce24b6
Ben Ford <ben.ford@puppetlabs.com>
2013-10-11 08:14:52 -0700

    No need for an empty placeholder file.

--- modules/advanced/manifests/mcollective/file.pp
+++ modules/advanced/manifests/mcollective/file.pp
@@ -7,9 +7,6 @@ define advanced::mcollective::file (
 ) {
   # This is the cached source name of the actual file
   $cachename = "${advanced::mcollective::config::cachedir}${name}"
-  # This is just an empty placeholder file, in case the master hasn't cached yet.
-  $modpath   = get_module_path('advanced')
-  $empty     = "${modpath}/files/empty.txt"
 
   # Compiling directly into the catalog is loads safer than an unauthenticated
   # fileserver mountpoint. Still, this would probably never be done in production.
@@ -18,6 +15,6 @@ define advanced::mcollective::file (
     owner   => $owner,
     group   => $group,
     mode    => $mode,
-    content => file($cachename, $empty),
+    content => file($cachename, '/dev/null'),
   }
 }
