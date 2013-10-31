52c2c9d487f8dd72950044b81275d7ea56397402
Ben Ford <ben.ford@puppetlabs.com>
2013-10-30 17:14:42 -0700

    Add license files so the master quits complaining

--- modules/fundamentals/manifests/master.pp
+++ modules/fundamentals/manifests/master.pp
@@ -14,6 +14,12 @@ class fundamentals::master ( $classes = [] ) {
     require => Service['pe-httpd'],
   }
 
+  # Write out our edu license file to prevent console noise
+  file { '/etc/puppetlabs/license.key':
+    ensure => file,
+    source => 'puppet:///modules/fundamentals/license.key',
+  }
+
   package { 'git':
     ensure => present,
   }
