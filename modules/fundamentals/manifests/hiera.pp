f9dd6df59e23ee3d60470eb1d2e67c3fb56abd59
Carthik Sharma <carthik@puppetlabs.com>
2013-12-20 12:09:11 -0800

    add a comment to state PE version compatibility

--- modules/fundamentals/manifests/hiera.pp
+++ modules/fundamentals/manifests/hiera.pp
@@ -1,6 +1,10 @@
 # Make sure that Hiera is configured for all nodes so that we
 # can work through the hiera sections without teaching them
 # how to configure it.
+
+# Note: this assume that the hiera.yaml file exists, which is 
+# the case on PE > 3.0.0. This will fail for older versions of PE
+
 class fundamentals::hiera {
   File {
     owner => 'root',
