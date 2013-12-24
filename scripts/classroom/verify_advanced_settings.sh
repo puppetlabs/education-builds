a07aa4741abda537e5e5006fded175b253ee8be7
Carthik Sharma <carthik@puppetlabs.com>
2013-12-23 17:58:44 -0800

    Fixes TRAINVM-46: check hostname validity
    See: https://tickets.puppetlabs.com/browse/PUP-677
    Puppet fails when the hostname, and thus the username on the master
      is set to all numerals.
    I decided to apply the test to advanced as well, since although we
      don't manage the ssh_authorized_keys it is possible that something
      that relies on executing a command as a user might fail if we set
      the hostname and the username with all numeric characters.

--- scripts/classroom/verify_advanced_settings.sh
+++ scripts/classroom/verify_advanced_settings.sh
@@ -8,6 +8,12 @@ check "[ '`hostname`' == '${NAME}.puppetlabs.vm' ]"                 \
       "Checking hostname"                                           \
       "You should set the hostname to ${NAME}.puppetlabs.vm"
 
+# `hostname -s` should be alphanumeric, can contain underscores, and # contain at least one alphabetical character, and finally,
+# not be composed of all numerals:
+check "echo `hostname -s` | grep -Pq '^(?=.*[a-zA-Z])^[a-zA-Z0-9][a-zA-Z0-9_]+' " \
+      "Checking hostname validity"                                  \
+      "Hostnames should be alphanumeric with at least one alpabhetical character"
+
 check "grep HOSTNAME=${NAME}.puppetlabs.vm /etc/sysconfig/network"  \
       "Checking that the hostname will be set on boot"              \
       "You should set HOSTNAME=${NAME}.puppetlabs.vm in /etc/sysconfig/network"
