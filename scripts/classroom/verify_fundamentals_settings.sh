d7866da8088ee2f02e3976fc3023eb7e71b91a30
Carthik Sharma <carthik@puppetlabs.com>
2014-01-08 12:16:32 -0800

    update the regular expression for hostname validity

--- scripts/classroom/verify_fundamentals_settings.sh
+++ scripts/classroom/verify_fundamentals_settings.sh
@@ -9,11 +9,11 @@ check "[ '`hostname`' == '${NAME}.puppetlabs.vm' ]"                 \
       "You should set the hostname to ${NAME}.puppetlabs.vm"
 
 # `hostname -s` should be alphanumeric, can contain underscores, and 
-# contain at least one alphabetical character, and finally,
+# contain at least one alphabetical character, with no caps, and finally,
 # not be composed of all numerals:
-check "echo `hostname -s` | grep -Pq '^(?=.*[a-zA-Z])^[a-zA-Z0-9][a-zA-Z0-9_]+' " \
+check "echo `hostname -s` | grep -Pq '^(?=.*[a-z])\A[a-z0-9][a-z0-9._]+\z' " \
       "Checking hostname validity"                                  \
-      "Hostnames should be alphanumeric with at least one alpabhetical character"
+      "Hostnames should be alphanumeric with at least one alphabhetical character"
 
 check "grep HOSTNAME=${NAME}.puppetlabs.vm /etc/sysconfig/network"  \
       "Checking that the hostname will be set on boot"              \
