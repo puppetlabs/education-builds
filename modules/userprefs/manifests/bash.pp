4f087c1d085598ad848b8424a8cfad12786a9f30
Klynton Jessup <klynton@puppetlabs.com>
2014-02-04 10:59:03 -0700

    Updating bash shell and prompt.
    Added some coloring to the prompt. Displaying the full path to the `cwd`.
    Also listing which git branch the user currently has selected.

--- modules/userprefs/manifests/bash.pp
+++ modules/userprefs/manifests/bash.pp
@@ -5,6 +5,20 @@ class userprefs::bash (
     ensure => present,
   }
 
+  file { '/root/.bashrc':
+    ensure  => file,
+    replace => false,
+    source  => 'puppet:///modules/userprefs/shell/bashrc',
+    require => Package['bash'],
+  }
+
+  file { '/root/.bash_profile':
+    ensure  => file,
+    replace => false,
+    source  => 'puppet:///modules/userprefs/shell/bash_profile',
+    require => File['/root/.bashrc'],
+  }
+
   if $default {
     user { 'root':
       ensure  => present,
