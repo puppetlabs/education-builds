fbf5c30aa5cc13eb2d59cb6657c7bc0ad6a7c5ab
Ben Ford <ben.ford@puppetlabs.com>
2014-02-12 10:04:19 -0800

    This is a cleaner method

--- modules/fundamentals/manifests/console/patch.pp
+++ modules/fundamentals/manifests/console/patch.pp
@@ -1,8 +1,32 @@
 class fundamentals::console::patch {
-  # This would be much nicer with a diff type....
-  file_line { 'live management select none':
-    path  => '/opt/puppet/share/live-management/public/javascripts/node.js',
-    line  => "              node.set('isSelected', false);",
-    match => '^\s*node.set\(\'isSelected',
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+  Exec {
+    cwd     => '/opt/puppet/share/live-management',
+    path    => '/usr/bin',
+    creates => '/tmp/patches/lock',
+    before  => File['/tmp/patches/lock'],
+  }
+
+  file { '/tmp/patches':
+    ensure => directory,
+  }
+
+  file { '/tmp/patches/selectNone.diff':
+    ensure => file,
+    source => 'puppet:///modules/fundamentals/selectNone.diff',
+  }
+
+  exec { 'Live Management select none':
+    command => 'patch -p0 < /tmp/patches/selectNone.diff',
+    require => File['/tmp/patches/selectNone.diff'],
+  }
+
+  # completion flag
+  file { '/tmp/patches/lock':
+    ensure => file,
   }
 }
