1befbd4a7d56e4ce0182a1e3737669468b25544c
Ben Ford <ben.ford@puppetlabs.com>
2014-03-07 20:28:55 -0800

    missed a comma :(

--- modules/userprefs/manifests/defaults.pp
+++ modules/userprefs/manifests/defaults.pp
@@ -2,7 +2,7 @@ class userprefs::defaults {
   include userprefs::profile
 
   class { 'userprefs::bash':
-    password => '$1$hgIZHl1r$tEqMTzoXz.NBwtW3kFv33/'
+    password => '$1$hgIZHl1r$tEqMTzoXz.NBwtW3kFv33/',
     replace  => true,
   }
 
