7ec9223f48cbf93f6fcb4f157ad2fac3bf568c71
Ben Ford <ben.ford@puppetlabs.com>
2014-04-17 19:50:59 -0700

    Manage the transition to gitlab for rapid deploy

---
+++ modules/classroom/manifests/rapid_deploy.pp
@@ -0,0 +1,84 @@
+class classroom::rapid_deploy (
+  $gitserver,
+  $gitserver_ip,
+) {
+  if ! is_ip_address($gitserver_ip) {
+    fail('Please pass a valid IP address to the rapid_deploy class')
+  }
+
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  host { $gitserver:
+    ip => $gitserver_ip,
+  }
+
+  class { 'r10k':
+    sources => {
+      'puppet' => {
+        'remote'  => "git@${gitserver}:root/control.git",
+        'basedir' => "${::settings::confdir}/environments",
+        'prefix'  => false,
+      }
+    },
+    purgedirs         => ["${::settings::confdir}/environments"],
+    manage_modulepath => true,
+    modulepath        => "${::settings::confdir}/environments/\$environment/modules:/etc/puppetlabs/puppet/modules:/opt/puppet/share/puppet/modules",
+  }
+
+  augeas { 'purge extra environments':
+    context => '/files/etc/puppetlabs/puppet/puppet.conf',
+    changes => "rm *[label() != 'main' and label() != 'agent' and label() != 'master']",
+  }
+
+  file { '/etc/init.d/webhooks':
+    ensure => file,
+    source => 'puppet:///modules/classroom/webhooks.init',
+    before => Service['webhooks'],
+  }
+
+  file { '/usr/local/bin/webhooks':
+    ensure  => file,
+    content => template('classroom/webhooks.erb'),
+    notify  => Service['webhooks'],
+  }
+
+  package { 'sinatra':
+    ensure   => present,
+    provider => gem,
+    before   => Service['webhooks'],
+  }
+
+  service { 'webhooks':
+    ensure => running,
+    enable => true,
+  }
+
+  file { '/root/.ssh':
+    ensure => directory,
+    mode   => '0600',
+  }
+  file { '/root/.ssh/config':
+    ensure => file,
+    mode   => '0600',
+    source => 'puppet:///modules/classroom/gitlab/sshconfig',
+  }
+  file { '/root/.ssh/gitlab_rsa':
+    ensure => file,
+    mode   => '0600',
+    source => 'puppet:///modules/classroom/gitlab/gitlab_rsa',
+  }
+  file { '/root/.ssh/gitlab_rsa.pub':
+    ensure => file,
+    mode   => '0600',
+    source => 'puppet:///modules/classroom/gitlab/gitlab_rsa.pub',
+  }
+  file { '/root/.gitconfig':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/classroom/gitlab/gitconfig',
+  }
+}
---
+++ modules/classroom/tests/rapid_deploy.pp
@@ -0,0 +1,4 @@
+class { 'classroom::rapid_deploy':
+  gitserver    => 'gitlab.puppetlabs.vm',
+  gitserver_ip => '172.16.144.140',
+}
