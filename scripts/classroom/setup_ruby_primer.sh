8c4d260ed29452a9aa95e5a20aa666c60237b4a3
Klynton Jessup <klynton@puppetlabs.com>
2014-04-25 21:14:19 -0600

    Fix the ruby setup script.
    This now only installs the correct ruby libs and supporting RPMs.

--- scripts/classroom/setup_ruby_primer.sh
+++ scripts/classroom/setup_ruby_primer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ ! -f /root/.extendingrc ]; then
   echo "Installing the Puppet Enterprise ruby packages."
-  yum localinstall --disablerepo=* /root/puppet-enterprise/packages/el-6-i386/pe-ruby*
+  cd /root/puppet-enterprise/packages/el-6-i386/
+  yum localinstall --disablerepo=* pe-augeas*.rpm pe-libevent*.rpm pe-libyaml*.rpm pe-ruby-1.9.3*.rpm pe-ruby-augeas*.rpm pe-bundler*.rpm
   touch .extendingrc
   echo "Please run 'exec bash' or 'exec zsh' to update your PATH."
 else
   echo "This script has already installed the required packages. Please try 'exec bash' or 'exec zsh' to set your PATH."
 fi
-
