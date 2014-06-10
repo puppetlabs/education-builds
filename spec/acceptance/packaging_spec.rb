1cb34cd09147a32197eb95ce74503176821f4773
Carthik Sharma <carthik@puppetlabs.com>
2014-06-10 15:34:29 -0700

    bootstrap init.pp covered

--- spec/acceptance/packaging_spec.rb
+++ spec/acceptance/packaging_spec.rb
@@ -1,35 +1,72 @@
 #! /usr/bin/env ruby -S rspec
 require 'spec_helper_acceptance'
 
-if hosts_as('learning').length > 0
-  describe 'PE installer' do
-    it 'should be cached' do
-      file('/root/puppet-enterprise').should be_directory
-    end
+describe 'PE installer' do
+  it 'should be cached' do
+    file('/root/puppet-enterprise').should be_directory
   end
-  describe 'Puppetlabs yumrepo' do
-    it 'should be disabled' do
-      expect(shell('yum repolist disabled').stdout).to match /puppetlabs/     
-    end
+end
+describe 'Puppetlabs yumrepo' do
+  it 'should be disabled' do
+    expect(shell('yum repolist disabled').stdout).to match /puppetlabs/     
   end
-#  describe 'Local and remote CentOS yum repos' do
-#    it 'should be enabled' do
-#      on hosts, "yum repolist enabled" do |r|
-#        expect(r.stdout).to match /^base\b/
-#        expect(r.stdout).to match /^epel\b/
-#        expect(r.stdout).to match /^extras\b/
-#        expect(r.stdout).to match /^updates\b/
-#        expect(r.stdout).to match /^base_local\b/
-#        expect(r.stdout).to match /^epel_local\b/
-#        expect(r.stdout).to match /^updates_local\b/
-#      end
+end
+#describe 'Local and remote CentOS yum repos' do
+#  it 'should be enabled' do
+#    on hosts, "yum repolist enabled" do |r|
+#      expect(r.stdout).to match /^base\b/
+#      expect(r.stdout).to match /^epel\b/
+#      expect(r.stdout).to match /^extras\b/
+#      expect(r.stdout).to match /^updates\b/
+#      expect(r.stdout).to match /^base_local\b/
+#      expect(r.stdout).to match /^epel_local\b/
+#      expect(r.stdout).to match /^updates_local\b/
 #    end
 #  end
-#  describe 'yum.conf' do
-#     it 'should skip unavailable repos' do
-#       file('/etc/yum.conf').should contain('skip_if_unavailable=1')
-#     end
-#  end
+#end
+#describe 'yum.conf' do
+#   it 'should skip unavailable repos' do
+#     file('/etc/yum.conf').should contain('skip_if_unavailable=1')
+#   end
+#end
+describe file('/usr/bin/envpuppet') do
+  it { should be_mode 755 }
+end
+describe file('/root/.ip_info.sh') do
+  it { should be_mode 755 }
+end
+describe package('patch') do
+  it {should be_installed }
+end
+describe package('screen') do
+  it {should be_installed }
+end
+describe package('telnet') do
+  it {should be_installed }
+end
+describe package('rubygems') do
+  it {should be_installed }
+end
+describe package('tree') do
+  it {should be_installed }
+end
+describe host('localhost.localdomain') do
+    it { should be_resolvable.by('hosts') }
+end
+describe file('/etc/puppet/ssl') do
+  it {should_not be_directory }
+end
+describe file('/etc/ssh/sshd_config') do
+  it { should contain('^GSSAPIAuthentication no') }
 end
 
+# Learning VM specific stuff:
+if hosts_as('learning').length > 0
+end
 
+# Training VM specific stuff below:
+if hosts_as('training').length > 0
+  describe file('/root/.ssh_keygen.sh') do
+    it { should be_mode 755 }
+  end
+end
