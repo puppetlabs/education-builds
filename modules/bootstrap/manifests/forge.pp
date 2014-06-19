ce7a47abecf4b366b23bd874195a995a8d6e10c5
Carthik Sharma <carthik@puppetlabs.com>
2014-06-19 13:17:56 -0700

    update the type to now use the function

--- modules/bootstrap/manifests/forge.pp
+++ modules/bootstrap/manifests/forge.pp
@@ -6,25 +6,19 @@ define bootstrap::forge(
   $cache_dir = '/usr/src/forge',
   $version   = undef
 ) {
-
-  if $version {
-    $ver = $version
-  } else {
-    # Hideous hack to ask puppet/face for latest version, assumes only one result, and takes first version.
-    $ver = inline_template("<% require 'puppet/face'-%><%=
-begin
-  Puppet::Face[:module,'1.0.0'].search('${name}').first['releases'].first['version']
-rescue
-  nil
-end -%>")
-    if !$ver { fail("Unable to determine ${name} module version.") }
-  }
-
   $mod = split($name, '[-/]')
   $author = $mod[0]
   $module = $mod[1]
+  
+  if $version {
+    $ver = $version
+  } else {
+    $ver = latest_module_version($author, $module)
+    if !$ver { fail("Unable to determine ${author}-${name}'s version." }
+  }
+
   $filename = "${author}-${module}-${ver}.tar.gz"
-  $url      = "http://forge.puppetlabs.com/${author}/${module}/${ver}.tar.gz"
+  $url      = "https://forgeapi.puppetlabs.com/v3/files/${filename}"
 
   exec { "curl ${name}":
     command => "curl -o ${cache_dir}/${filename} $url",
