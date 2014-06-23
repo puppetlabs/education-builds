29843612aa2a122db09c354107171dc211776522
Carthik Sharma <carthik@puppetlabs.com>
2014-06-23 13:55:00 -0700

    training VM specific tests

--- spec/acceptance/packaging_spec.rb
+++ spec/acceptance/packaging_spec.rb
@@ -270,4 +270,10 @@ if hosts_as('training').length > 0
   describe file('/root/.ssh_keygen.sh') do
     it { should be_mode 755 }
   end
+  describe 'WordPress installer' do
+    it 'should be cached' do
+      expect(shell("file /usr/src/wordpress/wordpress*.tar.gz").stdout).to match /gzip compressed data/
+    end
+  end
 end
+
