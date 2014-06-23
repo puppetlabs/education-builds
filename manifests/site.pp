4e775cb3f0a06554ece0f022b03555f14466d8fd
Carthik Sharma <carthik@puppetlabs.com>
2014-06-23 11:44:06 -0700

    add vagrant setup to learning VM too

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,14 @@
 node default {
   include bootstrap
   include localrepo
-  include training
   include vagrant
+  include training
 }
 
 node /learn/ {
   include bootstrap
   include localrepo
+  include vagrant
   include learning
   stage { 'pe_install': require => Stage['main'], }
   class { 'learning::install': stage => pe_install, }
