1d7b9e64815598c2011847c1654bf2168aee4e48
Carthik Sharma <carthik@puppetlabs.com>
2014-06-23 09:56:49 -0700

    Updated list of modules to be cached. Some cached by PE now, some not needed

--- modules/bootstrap/manifests/cache_modules.pp
+++ modules/bootstrap/manifests/cache_modules.pp
@@ -17,15 +17,11 @@ class bootstrap::cache_modules(
   bootstrap::forge { 'puppetlabs-concat':
     version => '1.0.0',
   }
-
   bootstrap::forge { 'camptocamp-augeasfacter':
     version => '0.1.0',
   }
   bootstrap::forge { 'domcleal-augeasproviders':
     version => '1.0.0',
-  }
-  bootstrap::forge { 'cprice404-inifile':
-    version => '0.10.4',
   }
   bootstrap::forge { 'razorsedge-vmwaretools':
     version => '4.4.1',
@@ -42,41 +38,32 @@ class bootstrap::cache_modules(
   bootstrap::forge { 'thias-vsftpd':
     version => '0.1.2',
   }
-  bootstrap::forge { 'puppetlabs-firewall':
-    version => '0.4.1',
-  }
-  #  bootstrap::forge { 'saz-sudo':
-  # version => '2.2.0',
-  #}
   bootstrap::forge { 'puppetlabs-vcsrepo':
     version => '0.1.2',
   }
   bootstrap::forge { 'puppetlabs-ntp':
     version => '3.0.1',
   }
-
-  # These are the additional modules needed by the Advanced course
-  # puppetlabs-mysql is also needed
-  bootstrap::forge { 'zack-haproxy':
-    version => '0.2.0',
+  bootstrap::forge { 'puppetlabs-haproxy':
+    version => '0.5.0',
   }
-  bootstrap::forge { 'zack-irc':
-    version => '0.0.6',
+  bootstrap::forge { 'jamtur01-irc':
+    version => '0.0.7',
   }
   bootstrap::forge { 'hunner-charybdis':
-    version => '0.2.0',
+    version => '1.0.0',
   }
   bootstrap::forge { 'puppetlabs-puppetdb':
     version => '1.6.0',
   }
-  bootstrap::forge { 'puppetlabs-apt':
-    version => '1.2.0',
-  }
-  bootstrap::forge { 'puppetlabs-postgresql':
-    version => '2.4.1',
-  }
   bootstrap::forge { 'puppetlabs-pe_gem':
     version => '0.0.1',
+  }
+  bootstrap::forge { 'stahnma-epel':
+    version => '0.0.6',
+  }
+  bootstrap::forge { 'nanliu-staging':
+    version => '0.4.0',
   }
 
 
