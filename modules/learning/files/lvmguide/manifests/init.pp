80ab50081fe3571fe2834ebcca67c67a4023a697
Carthik Sharma <carthik@puppetlabs.com>
2014-08-13 11:11:52 -0700

    reorg of how the lvm guide website is set up and managed

--- modules/learning/files/lvmguide/manifests/init.pp
+++ modules/learning/files/lvmguide/manifests/init.pp
@@ -2,19 +2,14 @@ class lvmguide (
   $document_root = '/var/www/html/lvmguide',
   $port          = '80',
 ) {
+
+  # Manage apache, the files for the website will be 
+  # managed by the quest tool
   class { 'apache': 
     default_vhost => false,
   }
   apache::vhost { 'learning.puppetlabs.vm':
     port    => $port,
     docroot => $document_root,
-  }
-  file { '/var/www/html/lvmguide':
-    ensure  => directory,
-    owner   => $::apache::params::user,
-    group   => $::apache::params::group,
-    source  => 'puppet:///modules/lvmguide/html',
-    recurse => true,
-    require => Class['apache'],
   }
 }
