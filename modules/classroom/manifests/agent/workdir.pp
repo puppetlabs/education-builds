4670021f3a41a2bf7b820ba86b554b0c42c8e3e0
beergeek <brett.gray@puppetlabs.com>
2014-09-05 15:33:19 +1000

    Update workdir.pp

--- modules/classroom/manifests/agent/workdir.pp
+++ modules/classroom/manifests/agent/workdir.pp
@@ -35,7 +35,7 @@ define classroom::agent::workdir (
         replace => false,
       }
 
-      file { "/etc/puppetlabs/puppet/environments/production/environment.conf":
+      file { "${workdir}/environment.conf":
         ensure  => file,
         content => "environment_timeout = 0\n",
         replace => false,
