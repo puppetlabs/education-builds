78a1dde7f730457bc5021298b1058e25c43ff82c
Klynton Jessup <klynton@puppetlabs.com>
2014-10-02 13:42:38 -0700

    Just set the hosts with Puppet.

--- modules/training/manifests/init.pp
+++ modules/training/manifests/init.pp
@@ -10,6 +10,21 @@ class training {
   file { '/usr/src/wordpress':
     ensure => directory,
   }
+
+  host { 'training.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['training', 'localhost', 'localhost.localdomain'],
+    ip           => '127.0.0.1',
+    target       => '/etc/hosts',
+  }
+
+  host { 'localhost':
+    ensure       => 'present',
+    host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
+    ip           => '::1',
+    target       => '/etc/hosts',
+  }
+
   exec { 'Cache WordPress':
     cwd       => '/usr/src/wordpress',
     command   => '/usr/bin/wget --no-clobber https://www.wordpress.org/wordpress-3.8.tar.gz',
