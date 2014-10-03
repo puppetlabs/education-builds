eb9bd716ef4006f0e8958eabd584769ab994d7dc
Ben Ford <ben.ford@puppetlabs.com>
2014-10-02 20:33:20 -0700

    Script to build the virtual master ami
    Still don't have a good solution for Windows.

---
+++ cloud/virtual/win_essentials/setup/setup.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+#optional major version number, defaults to 7
+DISTRO=${1:-"7"}
+PASSWORD="puppet"
+VERSION="3.3.2"
+BUILD="puppet-enterprise-${VERSION}-el-${DISTRO}-x86_64"
+FILENAME="${BUILD}.tar.gz"
+
+curl -o /root/${FILENAME} https://s3.amazonaws.com/pe-builds/released/${VERSION}/${FILENAME}
+tar -xvzf /root/${FILENAME}
+cp answers.txt /root/${BUILD}/
+
+sed -i "s/^PasswordAuthentication no$/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sed -i "s/^PermitRootLogin.*$/PermitRootLogin yes/" /etc/ssh/sshd_config
+service sshd restart
+
+echo "root:${PASSWORD}" | chpasswd
+cp 99_puppetlabs.cfg /etc/cloud/cloud.cfg.d/
+
+cp vimrc /root/.vimrc
+cp -a vim /root/.vim
+
+yum update -y
+yum install vim tree git -y
+
+# for clean up
+cp bash_logout /home/ec2-user/.bash_logout
