860cb6cf8567342f61438b20b94a1dc23596d8f7
Jordan Olshevski <jordan@puppetlabs.com>
2014-10-09 12:00:20 -0700

    Add Windows Git Installer

--- modules/classroom/manifests/agent/git.pp
+++ modules/classroom/manifests/agent/git.pp
@@ -15,7 +15,24 @@ class classroom::agent::git {
     path        => $path,
   }
 
-  include ::git
+  if $::osfamily == 'windows' {
+    file { 'c:/git_install.exe':
+      ensure => present,
+      source => 'puppet:///modules/classroom/Git-1.9.4-preview20140929.exe',
+      before => Exec['install git'],
+    }
+    exec { 'install git':
+      command => 'c:/git_install.exe /VERYSILENT',
+      creates => 'C:\Program Files (x86)\Git'
+      path    => $::path,
+      before  => Exec['generate key'],
+    }
+  }
+  else {
+    class { '::git':
+      before => Exec['generate key'],
+    }
+  }
 
   file { $sshpath:
     ensure => directory,
@@ -25,16 +42,15 @@ class classroom::agent::git {
   exec { 'generate_key':
     command => "ssh-keygen -t rsa -N '' -f ${sshpath}",
     creates => "${sshpath}/id_rsa",
-    require => File[$sshpath],
   }
 
   exec { "git config --global user.name '${::hostname}'":
     unless  => 'git config --global user.name',
-    require => Class['::git'],
+    require => Exec['generate key'],
   }
 
   exec { "git config --global user.email ${::hostname}@puppetlabs.vm":
     unless  => 'git config --global user.email',
-    require => Class['::git'],
+    require => Exec['generate key'],
   }
 }
