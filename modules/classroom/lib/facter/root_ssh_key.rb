5b40e52abb21f6e773dd87a7b535153b31d76038
Jordan Olshevski <jordan@puppetlabs.com>
2014-10-09 15:31:27 -0700

    Move setcode Block Inside of Conditional
    Otherwise the first attempt returns the value as undef

--- modules/classroom/lib/facter/root_ssh_key.rb
+++ modules/classroom/lib/facter/root_ssh_key.rb
@@ -1,8 +1,10 @@
 Facter.add('root_ssh_key') do
   pubkey = '/root/.ssh/id_rsa.pub', 'C:/Users/Administrator/.ssh.pub'
   pubkey.each do |key|
-    setcode do
-      File.read(key).split[1] if File.exists? key
+    if File.exists? key
+      setcode do
+        File.read(key).split[1]
+      end
     end
   end
 end
