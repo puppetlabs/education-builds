add919da455fa55c6b1ba2809a70d71d7bbd6cbf
Jordan Olshevski <jordan@puppetlabs.com>
2014-10-09 15:53:46 -0700

    Windows File Path Correction

--- modules/classroom/lib/facter/root_ssh_key.rb
+++ modules/classroom/lib/facter/root_ssh_key.rb
@@ -1,5 +1,5 @@
 Facter.add('root_ssh_key') do
-  pubkey = '/root/.ssh/id_rsa.pub', 'C:/Users/Administrator/.ssh.pub'
+  pubkey = '/root/.ssh/id_rsa.pub', 'C:/Program Files (x86)/Git/.ssh/id_rsa.pub'
   pubkey.each do |key|
     if File.exists? key
       setcode do
