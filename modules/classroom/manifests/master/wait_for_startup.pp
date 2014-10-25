2c32f47fdbae5d5d1c69af730039619c5b69a24a
Ben Ford <ben.ford@puppetlabs.com>
2014-10-24 18:06:43 -0700

    Whoops! Forgot the path.
    No longer an infinite loop ;)

--- modules/classroom/manifests/master/wait_for_startup.pp
+++ modules/classroom/manifests/master/wait_for_startup.pp
@@ -1,9 +1,9 @@
 # The new PE stack takes a very long time to startup, which can cause
 # disconcerting errors. This simply schedules that to the end of the run
 # and waits for the service to resume servicing requests before allowing
-# the run to complete This ensures that if services are restarted, that
+# the run to complete. This ensures that if services are restarted, that
 # will happen *after* any resources that depend on them to be running
-# are enforced.
+# are enforced and that the service is available to receive the report.
 #
 # Affected resources:
 #   resources that use Console APIs
@@ -18,10 +18,10 @@ class classroom::master::wait_for_startup {
   Classroom::Console::Groupparam <| |> -> Service['pe-console-services']
 
   exec { 'wait for PE stack to startup':
-    path        => '/usr/bin', # make the agent happy :)
+    path        => '/opt/puppet/bin:/bin:/usr/bin',
     command     => "while true; do puppet status ${clientcert} --terminus rest && break; sleep 5; done",
     subscribe   => Service['pe-puppetserver', 'pe-console-services', 'pe-puppetdb'],
     refreshonly => true,
+    provider    => 'shell',
   }
-
 }
