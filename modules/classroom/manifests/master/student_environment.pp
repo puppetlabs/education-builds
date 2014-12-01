1522fb945b669866e26c5524bc002bfe71d0b985
Ben Ford <ben.ford@puppetlabs.com>
2014-12-01 10:59:37 -0800

    Fix management of environmentpath
    The `puppet_enterprise` module manages only the directory portion of
    the `environmentpath`, not any of the contents. We want to put starting
    default content in place. Unfortunately, the `environmentpath` is
    managed only when classified in the *PE Masters* nodegroup, so we cannot
    put contents into it until it's classified.
    
    This introduces a possible parse order problem, but it will fail in the
    direction of no action rather than a compile or enforcement failure.
    
    Thanks to @beergeek for identifying the problem.

--- modules/classroom/manifests/master/student_environment.pp
+++ modules/classroom/manifests/master/student_environment.pp
@@ -12,36 +12,39 @@ class classroom::master::student_environment inherits classroom::params {
     mode  => '0644',
   }
 
-  file { [
-    $environmentpath,
-    "${environment}",
-    "${environment}/manifests",
-    "${environment}/modules",
-  ]:
-    ensure => directory,
-  }
+  # we need this check because the $environmentpath doesn't exist until the
+  # node is classified as a PE Master, so we can't put files into it.
+  if defined(Class['puppet_enterprise::profile::master') {
+    file { [
+      $environment,
+      "${environment}/manifests",
+      "${environment}/modules",
+    ]:
+      ensure => directory,
+    }
 
-  file { "${environment}/manifests/site.pp":
-    ensure  => file,
-    source  => 'puppet:///modules/classroom/site.pp',
-    replace => false,
-  }
+    file { "${environment}/manifests/site.pp":
+      ensure  => file,
+      source  => 'puppet:///modules/classroom/site.pp',
+      replace => false,
+    }
 
-  # Ensure the environment cache is disabled and restart if needed
-  ini_setting {'environment_timeout':
-    ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'main',
-    setting => 'environment_timeout',
-    value   => '0',
-  }
+    # Ensure the environment cache is disabled and restart if needed
+    ini_setting {'environment_timeout':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'main',
+      setting => 'environment_timeout',
+      value   => '0',
+    }
 
-  # Ensure the environmentpath is configured and restart if needed
-  ini_setting {'environmentpath':
-    ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'main',
-    setting => 'environmentpath',
-    value   => $environmentpath,
+    # Ensure the environmentpath is configured and restart if needed
+    ini_setting {'environmentpath':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'main',
+      setting => 'environmentpath',
+      value   => $environmentpath,
+    }
   }
 }
