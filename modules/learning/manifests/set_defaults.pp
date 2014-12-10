cc03cd4d5164595b7547553e7a49bbbafbe9ef0a
Josh Samuelson <js@puppetlabs.com>
2014-12-09 16:13:05 -0800

    saner low mem settings and directory for defaults to live

--- modules/learning/manifests/set_defaults.pp
+++ modules/learning/manifests/set_defaults.pp
@@ -1,9 +1,14 @@
 # Add default memory settings after PE install
 
 class learning::set_defaults {
+  file { '/etc/puppetlabs/puppet/hieradata':
+    ensure => directory,
+  }
   file { '/etc/puppetlabs/puppet/hieradata/defaults.yaml':
     ensure => present,
     source => 'puppet:///modules/learning/defaults.yaml',
+    require => File['/etc/puppetlabs/puppet/hieradata']
+    require => Class['learning']
   }
 }
 
