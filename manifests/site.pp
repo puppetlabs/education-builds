de79a2cf7a257fe861dd0184349364ff9245c302
Josh Samuelson <js@puppetlabs.com>
2014-12-12 15:45:05 -0800

    Adding swap for low memory LVMs

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 node default {
   class { 'staging':
-     path  => '/usr/src/installer/',
-     owner => 'root',
-     group => 'root',
-   }
+    path  => '/usr/src/installer/',
+    owner => 'root',
+    group => 'root',
+  }
   class { 'bootstrap::get_pe': version => '3.7.0' }
   class { 'epel': epel_enabled => false }
   include bootstrap
@@ -16,11 +16,18 @@ node /student/ {
 }
 
 node /learn/ {
+  if $::memorysize_mb <= 4096 {
+    class { 'swap_file':
+      swapfile     => '/swapfile/swap1',
+      swapfilesize => '4.0 GB',
+    }
+  }
+
   class { 'staging':
-     path  => '/usr/src/installer/',
-     owner => 'root',
-     group => 'root',
-   }
+    path  => '/usr/src/installer/',
+    owner => 'root',
+    group => 'root',
+  }
   class { 'bootstrap::get_pe': version => '3.7.0' }
   include epel
   include bootstrap
