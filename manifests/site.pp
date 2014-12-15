fedb6b56b70505f71b8f6970833c19f58d303a8d
Josh Samuelson <js@puppetlabs.com>
2014-12-15 11:09:56 -0800

    PE 3.7.1 for testing

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,7 +29,7 @@ node /learn/ {
     owner => 'root',
     group => 'root',
   }
-  class { 'bootstrap::get_pe': version => '3.7.0' }
+  class { 'bootstrap::get_pe': version => '3.7.1' }
   include epel
   include bootstrap
   include localrepo
