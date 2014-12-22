b00d357f30083f2fb8fa757cc867a762928100af
Josh Samuelson <js@puppetlabs.com>
2014-12-22 15:43:06 -0800

    LVM packer build files

--- packer/scripts/learning_build.sh
+++ packer/scripts/learning_build.sh
@@ -1,8 +1,14 @@
+mkdir -p /opt/lvmguide
+mv /vagrant/file_cache/lvmguide-0.2.3.zip /opt/lvmguide
+
 yum install -y git yum-utils ruby-devel ruby rubygems
 gem install rake json
 
 cd /usr/src/
-git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
+git clone https://github.com/joshsamuelson/puppetlabs-training-bootstrap -b lvm/swap_fix
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake -f Rakefile.new learning
+
+rm -rf /root/.testing
+/root/bin/quest update
