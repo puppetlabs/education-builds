e68ae1f17f0ae5eb3c487b9cb117853c8d7c056d
Josh Samuelson <js@puppetlabs.com>
2014-12-29 17:37:46 -0800

    reverting agent.pp to master version for merge

--- modules/classroom/manifests/agent.pp
+++ modules/classroom/manifests/agent.pp
@@ -12,7 +12,7 @@ class classroom::agent (
   $consolepw   = $classroom::consolepw,
 ) inherits classroom {
   # A valid hostname is not necessarily a valid Puppet environment name!
-  validate_re($::clientcert, '^(?=.*[a-z])\A[a-z0-9][a-z0-9._]+\z', "The classroom environment supports lowercase alphanumeric hostnames only. ${name} is not a valid name. Please ask your instructor for assistance.")
+  validate_re($::hostname, '^(?=.*[a-z])\A[a-z0-9][a-z0-9._]+\z', "The classroom environment supports lowercase alphanumeric hostnames only. ${name} is not a valid name. Please ask your instructor for assistance.")
 
   # make sure our git environment is set up and usable
   include classroom::agent::git
@@ -30,7 +30,7 @@ class classroom::agent (
   # !!!! THIS WILL EXPORT AN EMPTY KEY ON THE FIRST RUN !!!!
   #
   # On the second run, the ssh key will exist and so this fact will be set.
-  @@classroom::user { $::clientcert:
+  @@classroom::user { $::hostname:
     key        => $::root_ssh_key,
     password   => $password,
     consolepw  => $consolepw,
@@ -41,7 +41,7 @@ class classroom::agent (
   if $managerepos {
     classroom::agent::workdir { $workdir:
       ensure   => present,
-      username => $::clientcert,
+      username => $::hostname,
       require  => Class['classroom::agent::git'],
     }
 
@@ -60,6 +60,8 @@ class classroom::agent (
 
   # windows goodies
   if $::osfamily  == 'windows' {
+    include classroom::agent::chocolatey
     include userprefs::npp
+    include classroom::agent::console2
   }
 }
