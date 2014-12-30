ef011a468a364edab2953f946f49555f2aceb3ac
Josh Samuelson <js@puppetlabs.com>
2014-12-30 11:42:56 -0800

    Pointing packer to main repo

--- packer/scripts/learning_build.sh
+++ packer/scripts/learning_build.sh
@@ -10,7 +10,7 @@ yum install -y git yum-utils ruby-devel ruby rubygems
 gem install rake json
 
 cd /usr/src/
-git clone https://github.com/joshsamuelson/puppetlabs-training-bootstrap -b lvm/swap_fix
+git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake -f Rakefile.new learning
