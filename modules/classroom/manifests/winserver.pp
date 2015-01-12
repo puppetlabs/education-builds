361f315b5d9bf44e49bb3a243ed1d9476c2cf34e
Josh Samuelson <js@puppetlabs.com>
2015-01-12 14:20:53 -0800

    missing commas

--- modules/classroom/manifests/winserver.pp
+++ modules/classroom/manifests/winserver.pp
@@ -9,13 +9,13 @@ class classroom::winserver inherits classroom::params {
     configureflag          => true,
     domaintype             => 'Forest',
     domain                 => 'forest',
-    domainname             => $classroom::params::ad_domainname             
-    netbiosdomainname      => $classroom::params::ad_netbiosdomainname      
+    domainname             => $classroom::params::ad_domainname, 
+    netbiosdomainname      => $classroom::params::ad_netbiosdomainname,
     domainlevel            => '6',
     forestlevel            => '6',
     installtype            => 'domain',
     installdns             => 'no',
-    dsrmpassword           => $classroom::params::ad_dsrmpassword           
+    dsrmpassword           => $classroom::params::ad_dsrmpassword,     
   }
 
 }
