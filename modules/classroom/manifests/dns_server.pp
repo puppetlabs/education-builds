a7bb870f6ab9bf56056650abb6e5f7d1015d8eaa
Josh Samuelson <js@puppetlabs.com>
2015-01-13 12:34:16 -0800

    Added dnserver defined type

---
+++ modules/classroom/manifests/dns_server.pp
@@ -0,0 +1,13 @@
+# Set a DNS server on the windows agents
+define classroom::dns_server (
+  $ip = '8.8.8.8',
+) {
+    # Only run on windows
+    if $::osfamily  == 'windows' {
+      exec { 'set_dns':
+        command   => "set-DnsClientServerAddress -interfacealias Ethernet0 -serveraddresses $ip",
+        unless    => "if ((Get-DnsClientServerAddress -addressfamily ipv4 -interfacealias Ethernet0).serveraddresses -match $ip)",
+        provider  => powershell,
+      }
+    }
+}
