bcca619c3575a426ae53b805c33ebd8956e1b922
Josh Samuelson <js@puppetlabs.com>
2015-01-13 14:35:18 -0800

    Manage certname

--- modules/classroom/manifests/agent.pp
+++ modules/classroom/manifests/agent.pp
@@ -37,6 +37,14 @@ class classroom::agent (
       data   => '0',
     }
     Classroom::Dns_server <<||>>
+    # Set certname 
+    ini_setting { "certname":
+      ensure  => present,
+      path    => 'C:\ProgramData\PuppetLabs\puppet\etc\puppet.conf',
+      section => 'main',
+      setting => 'certname',
+      value   => "${::hostname}.puppetlabs.vm",
+    }
   }
 
   # make sure our git environment is set up and usable
@@ -64,7 +72,7 @@ class classroom::agent (
 
   # if we are managing git repositories, then build out all this
   if $managerepos {
-    
+
     classroom::agent::workdir { $workdir:
       ensure   => present,
       username => $classroom::params::machine_name,
