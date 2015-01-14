7f9d97280096d47f0fa29864982461f248cb3ada
Josh Samuelson <js@puppetlabs.com>
2015-01-13 17:03:27 -0800

    Added AD machine quota setting

--- modules/classroom/manifests/winserver.pp
+++ modules/classroom/manifests/winserver.pp
@@ -25,6 +25,14 @@ class classroom::winserver inherits classroom::params {
     provider => powershell,
   }
 
+  # Increase the number of machines that a single user can join to the domain
+  exec { 'SetMachineQuota':
+    command   => 'get-addomain |set-addomain -Replace @{\'ms-DS-MachineAccountQuota\'=\'99\'}',
+    unless    => 'if ((get-addomain | get-adobject -prop \'ms-DS-MachineAccountQuota\' | select -exp \'ms-DS-MachineAccountQuota\') -lt 99) {exit 1}',
+    provider  => powershell,
+    require   => Class['windows_ad'],
+  }
+
   # Export AD server IP to be DNS server for agents
   @@classroom::dns_server { 'primary_ip':
     ip => $::ipaddress,
