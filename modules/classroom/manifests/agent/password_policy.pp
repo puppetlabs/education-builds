b36d212c3b871bcec2041d9c2a8eee77eb1364cf
Josh Samuelson <js@puppetlabs.com>
2015-01-16 13:32:42 -0800

    Improved password policy class

--- modules/classroom/manifests/agent/password_policy.pp
+++ modules/classroom/manifests/agent/password_policy.pp
@@ -5,32 +5,26 @@ class classroom::agent::password_policy (
   if $::osfamily == 'windows' {
     # Disable draconian password policy
     exec { 'ExportSecurityPolicy':
-      command     => 'secedit /export /cfg c:\windows\temp\secpol.cfg',
-      provider    => powershell,
-      creates     => 'c:/windows/temp/secpol.cfg',
+      command => 'secedit /export /cfg c:\windows\temp\secpol.cfg.orig',
+      provider => powershell,
+      creates => 'c:/windows/temp/secpol.cfg.orig',
     }
-    exec { 'EditPasswordComplexity':
-      command     => '(gc C:/windows/temp/secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0")',
-      unless      => 'if((gc C:/windows/temp/secpol.cfg |grep "PasswordComplexity").split("=")[1] -gt 0){exit 1}',
-      provider    => powershell,
-      subscribe   => Exec['ExportSecurityPolicy'],
-      refreshonly => true,
-    }
-    exec { 'EditPasswordLength':
-      command     => '(gc C:/windows/temp/secpol.cfg).replace("MinimumPasswordLength = *", "MinimumPasswordLength = 1")',
-      unless      => 'if((gc C:/windows/temp/secpol.cfg |grep "PasswordLength").split("=")[1] -gt 1){exit 1}',
-      provider    => powershell,
-      subscribe   => Exec['EditPasswordComplexity'],
+    exec { 'EditPasswordPolicy':
+      command => '((((gc C:/windows/temp/secpol.cfg.orig) -replace "PasswordComplexity.*", "PasswordComplexity = 0") -replace "MinimumPasswordLength.*", "MinimumPasswordLength = 1" ) -replace "PasswordHistory.*", "PasswordHistorySize = 0") | set-content c:/windows/temp/secpol.cfg',
+      creates => 'c:/windows/temp/secpol.cfg',
+      provider => powershell,
+      subscribe => Exec['ExportSecurityPolicy'],
       refreshonly => true,
     }
     exec { 'ApplySecurityPolicy':
-      command     => 'secedit /configure /db c:\windows\security\local.sdb /cfg c:\windows\temp\secpol.cfg /areas SECURITYPOLICY',
-      provider    => powershell,
-      subscribe   => Exec['EditPasswordLength'],
+      command => 'secedit /configure /db c:\windows\security\local.sdb /cfg c:\windows\temp\secpol.cfg /areas SECURITYPOLICY',
+      provider => powershell,
+      subscribe => Exec['EditPasswordPolicy'],
       refreshonly => true,
     }
-    else {
-      fail("The Password Policy Class supports only Windows, not ${::osfamily}")
-    }
   }
+  else {
+    fail("The Password Policy Class supports only Windows, not ${::osfamily}")
+  }
+
 }
