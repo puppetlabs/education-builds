042398c822700f0ce5062ac3ffacdddac36155d1
Josh Samuelson <js@puppetlabs.com>
2015-02-18 16:29:19 -0800

    Using swap drive for LVM

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node default {
     owner => 'root',
     group => 'root',
   }
-  class { 'bootstrap::get_pe': version => '3.7.1' }
+  class { 'bootstrap::get_pe': version => '3.7.2' }
   class { 'epel': epel_enabled => false }
   include bootstrap
   include localrepo
@@ -16,20 +16,12 @@ node /student/ {
 }
 
 node /learn/ {
-  if $::memorysize_mb <= 4096 {
-    class { 'swap_file':
-      swapfile     => '/swapfile',
-      swapfilesize => '4.0 GB',
-    }
-    include learning::set_swap
-  }
-
   class { 'staging':
     path  => '/usr/src/installer/',
     owner => 'root',
     group => 'root',
   }
-  class { 'bootstrap::get_pe': version => '3.7.1' }
+  class { 'bootstrap::get_pe': version => '3.7.2' }
   include epel
   include bootstrap
   include localrepo
