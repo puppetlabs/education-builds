6398e1ab66a2e452072e918cbb502196310543af
Josh Samuelson <js@puppetlabs.com>
2015-03-17 10:25:11 -0700

    Cleaning up output for packer

--- packer/scripts/student_build.sh
+++ packer/scripts/student_build.sh
@@ -1,5 +1,5 @@
 cd /usr/src/
-git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
+git clone https://github.com/casharma/puppetlabs-training-bootstrap -b r10kgems_update
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake student
