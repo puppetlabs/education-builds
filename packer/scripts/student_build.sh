2f64db033ef3f25c0dedcf1560240ab4604f9ad6
Josh Samuelson <js@puppetlabs.com>
2015-03-17 12:05:03 -0700

    Testing complete for student build

--- packer/scripts/student_build.sh
+++ packer/scripts/student_build.sh
@@ -1,5 +1,5 @@
 cd /usr/src/
-git clone https://github.com/casharma/puppetlabs-training-bootstrap -b r10kgems_update
+git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake student
