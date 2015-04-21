9cfce6c9f7e43c35e31a18441ff4777ce75fb28a
Josh Samuelson <js@puppetlabs.com>
2015-04-21 10:21:04 -0700

    Trying out centos7 for puppetfactory base

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,3 +37,14 @@ node /learn/ {
   include learning::install
   include learning::set_defaults
 }
+
+node /puppetfactory/ {
+  class { 'staging':
+    path  => '/usr/src/installer/',
+    owner => 'root',
+    group => 'root',
+  }
+  class { 'bootstrap::get_pe': 
+    version => '3.7.2' 
+  }  
+}
