dd148a25ba1c1723f7329d5db68dddb32ef181a9
Josh Samuelson <js@puppetlabs.com>
2015-05-04 11:39:41 -0700

    Cleaning up some cruft

--- packer/scripts/lms_cleanup.sh
+++ packer/scripts/lms_cleanup.sh
@@ -1,6 +1,27 @@
 #LMS VM cleanup scripts
+
+# Get puppet config ready
 PATH=/opt/puppet/bin:$PATH
 puppet config set certname ${HOSTNAME}
 puppet config set server ${HOSTNAME}
-puppet cert --generate ${HOSTNAME} --dns_alt_names "puppet,${HOSTNAME}" --verbose
+
+# Stop all PE processes to free up memory
+for s in `find /etc/init.d/ -name pe* -type f -printf "%f\n"`
+do
+  service $s stop
+done
+
+# Clean up PE installer files
+rm -rf /usr/src/puppet-enterprise*
+rm -rf /usr/src/puppet-enterprise
+rm -rf /usr/src/installer
+
+# Remove packages PE will regenerate agent installer
+rm -rf /opt/puppet/packages
+
+# Clean up other random files
+rm -rf /usr/src/puppetlabs-training-bootstrap
+rm -rf /usr/src/puppet
+rm -rf /usr/share/doc/*
+rm -rf /usr/src/kernels
 
