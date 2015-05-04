5aba02510adcaeac68cec1295f79172ffed72b89
Josh Samuelson <js@puppetlabs.com>
2015-05-04 11:10:00 -0700

    Add cert stuff to cleanup script

--- packer/scripts/lms_cleanup.sh
+++ packer/scripts/lms_cleanup.sh
@@ -1,1 +1,6 @@
 #LMS VM cleanup scripts
+PATH=/opt/puppet/bin:$PATH
+puppet config set certname ${HOSTNAME}
+puppet config set server ${HOSTNAME}
+puppet cert --generate ${HOSTNAME} --dns_alt_names "puppet,${HOSTNAME}" --verbose
+
