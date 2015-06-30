0d70fc8229a445cc0b517f960bbe8dbb1e18b0a7
Josh Samuelson <js@puppetlabs.com>
2015-06-30 11:45:34 -0700

    Trying something else

--- manifests/post.pp
+++ manifests/post.pp
@@ -1,27 +1,22 @@
-# Manifest for post build cleanup
+if $::hostname == "lms" {
+  # Put LMS files in place
+  include lms::lab_repo
+  include lms::course_selector
+}
+# Disable non-local yum repos
+yumrepo { [ 'updates', 'base', 'extras', 'epel']:
+  enabled  => '0',
+  priority => '99',
+  skip_if_unavailable => '1',
+}
 
-node default {
-  node /(lms|training)/ {
-    # Put LMS files in place
-    include lms::lab_repo
-    include lms::course_selector
-  }
-
-  # Disable non-local yum repos
-  yumrepo { [ 'updates', 'base', 'extras', 'epel']:
-    enabled  => '0',
-    priority => '99',
-    skip_if_unavailable => '1',
-  }
-
-  # Delete cruft left by install process
-  file { [
-    '/root/install.log',
-    '/root/install.log.syslog',
-    '/root/linux.iso',
-    '/root/post.log',
-    '/root/anaconda-ks.cfg'
-  ]:
-    ensure => absent,
-  }
+# Delete cruft left by install process
+file { [
+  '/root/install.log',
+  '/root/install.log.syslog',
+  '/root/linux.iso',
+  '/root/post.log',
+  '/root/anaconda-ks.cfg'
+]:
+  ensure => absent,
 }
