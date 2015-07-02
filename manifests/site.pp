e4d227f815ddb75559b750cf7f6b461d228d4645
Josh Samuelson <js@puppetlabs.com>
2015-07-02 16:55:01 -0700

    Paring down to just roles

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,27 +1,15 @@
 node default {
-  include epel
   include bootstrap::role::training
-  include localrepo
 }
-
 node /student/ {
-  include epel
   include bootstrap::role::student
 }
-
-
 node /learn/ {
-  include epel
-  include localrepo
   include bootstrap::role::learning
 }
-
 node /puppetfactory/ {
-  include epel
   include bootstrap::role::puppetfactory
-  include localrepo
 }
-
 node /lms/ {
   class { 'lms': }
 }
