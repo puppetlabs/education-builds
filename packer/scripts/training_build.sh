54ddb404c4bd5b999068d44cad2782c54258d543
Josh Samuelson <js@puppetlabs.com>
2015-07-23 15:52:53 -0700

    Updating build script for branch

--- packer/scripts/training_build.sh
+++ packer/scripts/training_build.sh
@@ -1,5 +1,5 @@
 cd /usr/src/
-git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
+git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap -b 2015.2_builds
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake training
