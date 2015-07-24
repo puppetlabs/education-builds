07db03ca03966c1b555ddcf3a25ea33869e3ed07
Josh Samuelson <js@puppetlabs.com>
2015-07-23 19:27:45 -0700

    Don't install broken root certs

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -1,8 +1,6 @@
 # Base install
 sudo yum install -y wget curl
 
-# Update root certs
-sudo wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
 
 sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 sudo sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
