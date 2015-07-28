4aefd6904598de5a24a21d2631f1a8c42d5ea8ea
Josh Samuelson <js@puppetlabs.com>
2015-07-28 08:42:08 -0700

    Remove test branch from build script

--- packer/scripts/training_build.sh
+++ packer/scripts/training_build.sh
@@ -1,5 +1,5 @@
 cd /usr/src/
-git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap -b 2015.2_builds
+git clone https://github.com/puppetlabs/puppetlabs-training-bootstrap
 cd /usr/src/puppetlabs-training-bootstrap/
 
 rake training
