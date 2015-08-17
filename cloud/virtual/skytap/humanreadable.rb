cd6bdfeb99f59d7b5088d0584e97f3674134cc28
Josh Samuelson <js@puppetlabs.com>
2015-08-17 14:45:46 -0700

    PE fallback environment setup scripts and tweaks to humanreadable output

--- cloud/virtual/skytap/humanreadable.rb
+++ cloud/virtual/skytap/humanreadable.rb
@@ -6,6 +6,7 @@ classroom = YAML.load(File.read(ARGV[0]))
 
 puts classroom['environment_name']
 puts '-------------------------'
+puts
 puts classroom['publish_sets'][0]['name'] + ":  "
 puts classroom['publish_sets'][0]['url'] + "  "
 puts classroom['publish_sets'][2]['name'] + ":  "
@@ -14,12 +15,14 @@ puts
 puts '-------------------------'
 puts
 
-classroom['vms'].each do |vm|
+classroom['vms'].sort_by{ |vm| vm['name'][/\d+/].to_i }.each do |vm|
   puts vm['name']
   puts '-------------------------'
   puts classroom['publish_sets'][1]['url'] + "   "
+  puts
   vm['services'].each do |service|
-    printf("port %i -- %s:%i  \n", service['internal_port'], service['external_ip'], service['external_port'])
+    printf(" * port %i - %s:%i  \n", service['internal_port'], service['external_ip'], service['external_port'])
   end
+  puts
   puts
 end
