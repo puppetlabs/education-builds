d538e35d3a01effd600390b58804ed69d2777f16
Josh Samuelson <js@puppetlabs.com>
2015-10-19 15:59:20 -0700

    Install a newer ruby for build

--- packer/scripts/pre_build.sh
+++ packer/scripts/pre_build.sh
@@ -1,4 +1,6 @@
-sudo yum install -y git yum-utils ruby-devel ruby rubygems
+sudo yum install -y git yum-utils rubygems
 sudo gem install rake json
 
-
+# Install new ruby
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=1.9.3
