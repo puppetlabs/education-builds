bb81632a0383bce32190334225be0b20e20846da
Josh Samuelson <js@puppetlabs.com>
2015-10-26 13:01:15 -0700

    Install augeas dependency

--- packer/scripts/training_build.sh
+++ packer/scripts/training_build.sh
@@ -1,9 +1,12 @@
+yum -y install augeas-devel
+
 # Install new ruby
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=1.9.3
 
 source /etc/profile.d/rvm.sh
 rvm install 1.9.3-dev
+gem install ruby-augeas
 
 cd /usr/src/
 
