7e7c60d251c1e6ff2f6826eab6ab46bf7cfd682f
Josh Samuelson <js@puppetlabs.com>
2016-04-18 11:46:41 -0700

    Alternate URLs in comments

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-`pwd`}
 MAIN_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-vmware-nocm-1.0.1.box}
-STUDENT_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-6.6-i386-vmware-puppet-1.0.3.box}
+#Public download URL: https://atlas.hashicorp.com/puppetlabs/boxes/centos-7.2-64-nocm/versions/1.0.1/providers/vmware_desktop.box
+STUDENT_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-6.6-i386-vmware-nocm-1.0.3.box}
+#Public download URL: https://atlas.hashicorp.com/puppetlabs/boxes/centos-6.6-32-nocm/versions/1.0.3/providers/vmware_desktop.box
 VAGRANT_BASE_URL=${VAGRANT_BASE_URL:-http://int-resources.ops.puppetlabs.net/Vagrant%20images}
 
 ## Set up default cache directories
@@ -25,7 +27,7 @@ fi
 
 echo Downloading main base image
 cd $BUILD_ROOT_DIR/output/education-base-vmware
-curl -O $VAGRANT_BASE_URL/$MAIN_VAGRANT_BOX_NAME 
+curl $VAGRANT_BASE_URL/$MAIN_VAGRANT_BOX_NAME -o $MAIN_VAGRANT_BOX_NAME
 tar xzvf $MAIN_VAGRANT_BOX_NAME
 mv *.vmx education-base.vmx
 
@@ -33,6 +35,6 @@ cd $BUILD_ROOT_DIR
 
 echo Downloading student base image
 cd $BUILD_ROOT_DIR/output/student-base-vmware
-curl -O $VAGRANT_BASE_URL/$STUDENT_VAGRANT_BOX_NAME 
+curl $VAGRANT_BASE_URL/$STUDENT_VAGRANT_BOX_NAME -o $STUDENT_VAGRANT_BOX_NAME
 tar xzvf $STUDENT_VAGRANT_BOX_NAME
 mv *.vmx student-base.vmx
