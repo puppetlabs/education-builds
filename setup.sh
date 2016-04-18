c67da91c1384d85de4233348a4d5d094cd166561
Josh Samuelson <js@puppetlabs.com>
2016-04-18 11:36:08 -0700

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-.}
-VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-vmware-nocm-1.0.1.box}
+BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-`pwd`}
+MAIN_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-vmware-nocm-1.0.1.box}
+STUDENT_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-6.6-i386-vmware-puppet-1.0.3.box}
 VAGRANT_BASE_URL=${VAGRANT_BASE_URL:-http://int-resources.ops.puppetlabs.net/Vagrant%20images}
 
 ## Set up default cache directories
@@ -15,13 +16,23 @@ fi
 if [ ! -d $BUILD_ROOT_DIR/output/education-base-vmware ]; then
     mkdir $BUILD_ROOT_DIR/output/education-base-vmware
 fi
+if [ ! -d $BUILD_ROOT_DIR/output/student-base-vmware ]; then
+    mkdir $BUILD_ROOT_DIR/output/student-base-vmware
+fi
 if [ ! -d $BUILD_ROOT_DIR/packer_cache ]; then
     mkdir $BUILD_ROOT_DIR/packer_cache
 fi
 
-echo Downloading base image
-cd output/education-base-vmware
-curl -O $VAGRANT_BASE_URL/$VAGRANT_BOX_NAME 
-tar xzvf $VAGRANT_BOX_NAME
+echo Downloading main base image
+cd $BUILD_ROOT_DIR/output/education-base-vmware
+curl -O $VAGRANT_BASE_URL/$MAIN_VAGRANT_BOX_NAME 
+tar xzvf $MAIN_VAGRANT_BOX_NAME
 mv *.vmx education-base.vmx
 
+cd $BUILD_ROOT_DIR
+
+echo Downloading student base image
+cd $BUILD_ROOT_DIR/output/student-base-vmware
+curl -O $VAGRANT_BASE_URL/$STUDENT_VAGRANT_BOX_NAME 
+tar xzvf $STUDENT_VAGRANT_BOX_NAME
+mv *.vmx student-base.vmx
