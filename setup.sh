a8d32ba9509851050135e4edacd3ec0b625233c5
Josh Samuelson <js@puppetlabs.com>
2016-04-18 15:15:54 -0700

    Clean up code duplication

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-`pwd`}
-MAIN_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-vmware-nocm-1.0.1.box}
+EDUCATION_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-vmware-nocm-1.0.1.box}
 #Public download URL: https://atlas.hashicorp.com/puppetlabs/boxes/centos-7.2-64-nocm/versions/1.0.1/providers/vmware_desktop.box
 STUDENT_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-6.6-i386-vmware-nocm-1.0.3.box}
 #Public download URL: https://atlas.hashicorp.com/puppetlabs/boxes/centos-6.6-32-nocm/versions/1.0.3/providers/vmware_desktop.box
@@ -10,39 +10,36 @@ VAGRANT_BASE_URL=${VAGRANT_BASE_URL:-http://int-resources.ops.puppetlabs.net/Vag
 ## Set up default cache directories
 echo Setting up default directories
 if [ ! -d $BUILD_ROOT_DIR/file_cache ]; then
-    mkdir $BUILD_ROOT_DIR/file_cache
+  mkdir $BUILD_ROOT_DIR/file_cache
 fi
 if [ ! -d $BUILD_ROOT_DIR/output ]; then
-    mkdir $BUILD_ROOT_DIR/output
+  mkdir $BUILD_ROOT_DIR/output
 fi
 if [ ! -d $BUILD_ROOT_DIR/packer_cache ]; then
-    mkdir $BUILD_ROOT_DIR/packer_cache
+  mkdir $BUILD_ROOT_DIR/packer_cache
 fi
 
+function get_vm {
+  IMAGE_TYPE=$1
+  IMAGE_NAME=`echo $IMAGE_TYPE | awk '{print toupper($0)}'`
+  IMAGE_BOX=${IMAGE_NAME}_VAGRANT_BOX_NAME
+  
+  rm -rf $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-vmware
+  mkdir $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-vmware
+  cd $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-vmware
 
-echo Downloading main base image
-rm -rf $BUILD_ROOT_DIR/output/education-base-vmware
-mkdir $BUILD_ROOT_DIR/output/education-base-vmware
-cd $BUILD_ROOT_DIR/output/education-base-vmware
-curl $VAGRANT_BASE_URL/$MAIN_VAGRANT_BOX_NAME -o $MAIN_VAGRANT_BOX_NAME
-tar xzvf $MAIN_VAGRANT_BOX_NAME
-mv *.vmx education-base.vmx
-echo Converting to single disk
-vmware-vdiskmanager -r disk-cl1.vmdk -t 5 disk-cl1.vmdk.temp
-rm -rf disk-cl1*vmdk
-mv disk-cl1.vmdk.temp disk-cl1.vmdk
+  echo Downloading $IMAGE_TYPE base image
+  curl $VAGRANT_BASE_URL/${!IMAGE_BOX} -o ${!IMAGE_BOX}
+  tar xzvf ${!IMAGE_BOX}
+  mv *.vmx education-base.vmx
 
-cd $BUILD_ROOT_DIR
+  echo Converting to single disk
+  vmware-vdiskmanager -r disk-cl1.vmdk -t 0 disk-cl1.vmdk.temp
+  rm -rf disk-cl1*vmdk
+  mv disk-cl1.vmdk.temp disk-cl1.vmdk
 
-echo Downloading student base image
-rm -rf $BUILD_ROOT_DIR/output/student-base-vmware
-mkdir $BUILD_ROOT_DIR/output/student-base-vmware
-cd $BUILD_ROOT_DIR/output/student-base-vmware
-rm -f $BUILD_ROOT_DIR/output/student-base-vmware/*
-curl $VAGRANT_BASE_URL/$STUDENT_VAGRANT_BOX_NAME -o $STUDENT_VAGRANT_BOX_NAME
-tar xzvf $STUDENT_VAGRANT_BOX_NAME
-mv *.vmx student-base.vmx
-echo Converting to single disk
-vmware-vdiskmanager -r disk-cl1.vmdk -t 5 disk-cl1.vmdk.temp
-rm -rf disk-cl1*vmdk
-mv disk-cl1.vmdk.temp disk-cl1.vmdk
+  cd $BUILD_ROOT_DIR
+}
+
+get_vm education
+get_vm student
