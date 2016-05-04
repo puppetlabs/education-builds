d0f5e50801a4344341985e00db9cb95cd6d65fbb
Josh Samuelson <js@puppetlabs.com>
2016-05-04 13:51:13 -0700

    Switch build to virtualbox

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,13 +1,8 @@
-# Make sure udev doesn't block our network
+# Make sure udev doesn't block our network on centos 6
 if uname -r | grep -q el6 ; then
   rm -rf /etc/udev/rules.d/70-persistent-net.rules
   rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
   sed -i -e "/^HWADDR.*/d" /etc/sysconfig/network-scripts/ifcfg-eth0
-else
-  # Workaround for Centos 7 bug that sets incorrect device name
-  export INTERFACE=`ip link | awk '/ens/{ gsub(":",""); print $2}'`
-  mv /etc/sysconfig/network-scripts/ifcfg-ens* /etc/sysconfig/network-scripts/ifcfg-$INTERFACE
-  sed -i "s/ens[0-9]\{2\}/$INTERFACE/" /etc/sysconfig/network-scripts/ifcfg=$INTERFACE
 fi
 rm -rf /dev/.udev/
 
