fb829ae134ec0a3074f1025b7e739d805c079775
Josh Samuelson <js@puppetlabs.com>
2016-05-06 12:32:06 -0700

    use quotes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-`pwd | tr -d '\n'`}
+BUILD_ROOT_DIR=${BUILD_ROOT_DIR:-`pwd`}
 EDUCATION_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-7.2-x86_64-virtualbox-nocm-1.0.1.box}
 #Public download URL: https://atlas.hashicorp.com/puppetlabs/boxes/centos-7.2-64-nocm/versions/1.0.1/providers/virtualbox_desktop.box
 STUDENT_VAGRANT_BOX_NAME=${VAGRANT_BOX_NAME:-centos-6.6-i386-virtualbox-nocm-1.0.3.box}
@@ -9,30 +9,30 @@ VAGRANT_BASE_URL=${VAGRANT_BASE_URL:-http://int-resources.ops.puppetlabs.net/Vag
 
 ## Set up default cache directories
 echo Setting up default directories
-if [ ! -d $BUILD_ROOT_DIR/file_cache ]; then
-  mkdir $BUILD_ROOT_DIR/file_cache
+if [ ! -d "$BUILD_ROOT_DIR/file_cache" ]; then
+  mkdir "$BUILD_ROOT_DIR/file_cache"
 fi
-if [ ! -d $BUILD_ROOT_DIR/file_cache/gems ]; then
-  mkdir $BUILD_ROOT_DIR/file_cache/gems
+if [ ! -d "$BUILD_ROOT_DIR/file_cache/gems" ]; then
+  mkdir "$BUILD_ROOT_DIR/file_cache/gems"
 fi
-if [ ! -d $BUILD_ROOT_DIR/file_cache/installers ]; then
-  mkdir $BUILD_ROOT_DIR/file_cache/installer
+if [ ! -d "$BUILD_ROOT_DIR/file_cache/installers" ]; then
+  mkdir "$BUILD_ROOT_DIR/file_cache/installer"
 fi
-if [ ! -d $BUILD_ROOT_DIR/output ]; then
-  mkdir $BUILD_ROOT_DIR/output
+if [ ! -d "$BUILD_ROOT_DIR/output" ]; then
+  mkdir "$BUILD_ROOT_DIR/output"
 fi
-if [ ! -d $BUILD_ROOT_DIR/packer_cache ]; then
-  mkdir $BUILD_ROOT_DIR/packer_cache
+if [ ! -d "$BUILD_ROOT_DIR/packer_cache" ]; then
+  mkdir "$BUILD_ROOT_DIR/packer_cache"
 fi
 
 function get_vm {
   IMAGE_TYPE=$1
-  IMAGE_NAME=`echo $IMAGE_TYPE | awk '{print toupper($0)} | tr -d "\n"'`
+  IMAGE_NAME=`echo $IMAGE_TYPE | awk '{print toupper($0)}'`
   IMAGE_BOX=${IMAGE_NAME}_VAGRANT_BOX_NAME
   
-  rm -rf $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox
-  mkdir $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox
-  cd $BUILD_ROOT_DIR/output/
+  rm -rf "$BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox"
+  mkdir "$BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox"
+  cd "$BUILD_ROOT_DIR/output/"
 
   if [ ! -s  ${!IMAGE_BOX} ]; then
     echo Downloading ${!IMAGE_BOX} base image
@@ -40,12 +40,12 @@ function get_vm {
   fi
   
   mv ${!IMAGE_BOX} $IMAGE_TYPE-base-virtualbox/
-  cd $BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox
+  cd "$BUILD_ROOT_DIR/output/$IMAGE_TYPE-base-virtualbox"
   tar xzvf ${!IMAGE_BOX}
   mv *.ovf $IMAGE_TYPE-base.ovf
 
-  mv ${!IMAGE_BOX} $BUILD_ROOT_DIR/output/
-  cd $BUILD_ROOT_DIR
+  mv ${!IMAGE_BOX} "$BUILD_ROOT_DIR/output/"
+  cd "$BUILD_ROOT_DIR"
 }
 
 get_vm education
