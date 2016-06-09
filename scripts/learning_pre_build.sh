d94f0aa19bfa179703af33f5ec43335f458850e8
kjhenner <kjhenner@gmail.com>
2016-06-09 13:56:41 -0700

    copy pe from the cache location to /tmp for learning_pre_build

--- scripts/learning_pre_build.sh
+++ scripts/learning_pre_build.sh
@@ -1,5 +1,9 @@
 hostnamectl set-hostname learning.puppetlabs.vm
 
+echo Build for PE version $PE_VERSION
+PE_PATH="/training/file_cache/installers/puppet-enterprise-${PE_VERSION}-el-7-x86_64.tar.gz"
+cp $PE_PATH /tmp/puppet-enterprise.tar.gz
+
 cd /usr/src/
 git clone https://github.com/puppetlabs/education-builds
 cd /usr/src/education-builds/scripts
