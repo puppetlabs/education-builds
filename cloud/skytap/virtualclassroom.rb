40f0d20e66330a7c7b34884c619a5a1ecf788b14
marrero984 <michael.marrero@puppetlabs.com>
2016-09-12 10:42:52 -0700

    change null to string

--- cloud/skytap/virtualclassroom.rb
+++ cloud/skytap/virtualclassroom.rb
@@ -29,7 +29,7 @@ def create_base_environment(name, base_template)
   env_config = {
     'template_id' => base_template, 
     'name' => name,
-    'suspend_on_idle' => null
+    'suspend_on_idle' => 'null'
   }
   return JSON.parse(RestClient.post(CONFIGURATION_URL, JSON.generate(env_config), HEADERS))
 end
