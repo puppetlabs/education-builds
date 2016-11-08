45151d93c249f6fb4ed6c45377cf32329c3056d6
Josh Samuelson <js@puppet.com>
2016-11-07 16:01:26 -0800

    Export ovas better

--- scripts/export-ova.sh
+++ scripts/export-ova.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-#if [[ $1 =~ \.vmx$ ]]; then
-# ovftool --targetType=ova --acceptAllEulas $1 output/puppet-$PE_VERSION-$VM_TYPE-$PTB_VERSION.ova
-#fi
+if [[ $1 =~ \.vmx$ ]]; then
+  openssl sha1 $1 > output/puppet-$PE_VERSION-$VM_TYPE-$PTB_VERSION.mf
+fi
 if [[ $1 =~ \.ovf$ ]]; then
-  sed -i '' 's/virtualbox\-2\.2/vmx-09/' $1
-  sed -i '' 's/MACAddress="[0-9A-F]*"/MACAddress=""/' $1
+  # Clean up ovf
+  if [ `uname` = "Darwin" ]; then
+    # Use BSD syntax on OSX
+    sed -i '' 's/virtualbox\-2\.2/vmx-09/' $1
+    sed -i '' 's/MACAddress="[0-9A-F]*"/MACAddress=""/' $1
+  else
+    sed -i 's/virtualbox\-2\.2/vmx-09/' $1
+    sed -i 's/MACAddress="[0-9A-F]*"/MACAddress=""/' $1
+  fi
+  # Remove previous build
   rm -f output/puppet-$PE_VERSION-$VM_TYPE-$PTB_VERSION.ova
+  # Recreate ova manifest after file changes
+  openssl sha1 $1 >> output/puppet-$PE_VERSION-$VM_TYPE-$PTB_VERSION.mf
   ovftool --targetType=ova --acceptAllEulas $1 output/puppet-$PE_VERSION-$VM_TYPE-$PTB_VERSION.ova
 fi
